0f784f8d3ee39360e770f090f929c699df0945fe
jtangGitHub <jtangGitHub@gmail.com>
2016-08-04 12:26:39 -0400

    initial checkin for passengers/timeline

--- gtas-parent/gtas-loader/src/main/java/gov/gtas/services/LoaderStatistics.java
+++ gtas-parent/gtas-loader/src/main/java/gov/gtas/services/LoaderStatistics.java
@@ -4,37 +4,64 @@
  * Please see LICENSE.txt for details.
  */
 package gov.gtas.services;
+
 /**
  * A Data structure to capture GTAS Loader Run statistics.
  */
 public class LoaderStatistics {
-    private int numFilesProcessed = 0;
-    private int numFilesAborted = 0;
-    private int numMessagesProcessed = 0;
-    private int numMessagesFailed = 0;
-    public int getNumFilesProcessed() {
-        return numFilesProcessed;
-    }
-    public void incrementNumFilesProcessed() {
-        this.numFilesProcessed++;
-    }
-    public int getNumFilesAborted() {
-        return numFilesAborted;
-    }
-    public void incrementNumFilesAborted() {
-        this.numFilesAborted++;
-    }
-    public int getNumMessagesProcessed() {
-        return numMessagesProcessed;
-    }
-    public void incrementNumMessagesProcessed(int incr) {
-        this.numMessagesProcessed += incr;
-    }
-    public int getNumMessagesFailed() {
-        return numMessagesFailed;
-    }
-    public void incrementNumMessagesFailed(int incr) {
-        this.numMessagesFailed += incr;
-    }
+	private int numFilesProcessed = 0;
+	private int numFilesAborted = 0;
+	private int numMessagesProcessed = 0;
+	private int numMessagesFailed = 0;
+
+	public int getNumFilesProcessed() {
+		return numFilesProcessed;
+	}
+
+	/**
+	 * Increment num files processed.
+	 */
+	public void incrementNumFilesProcessed() {
+		this.numFilesProcessed++;
+	}
+
+	public int getNumFilesAborted() {
+		return numFilesAborted;
+	}
+
+	/**
+	 * Increment num files aborted.
+	 */
+	public void incrementNumFilesAborted() {
+		this.numFilesAborted++;
+	}
+
+	public int getNumMessagesProcessed() {
+		return numMessagesProcessed;
+	}
+
+	/**
+	 * Increment num messages processed.
+	 *
+	 * @param incr
+	 *            the incr
+	 */
+	public void incrementNumMessagesProcessed(int incr) {
+		this.numMessagesProcessed += incr;
+	}
+
+	public int getNumMessagesFailed() {
+		return numMessagesFailed;
+	}
+
+	/**
+	 * Increment num messages failed.
+	 *
+	 * @param incr
+	 *            the incr
+	 */
+	public void incrementNumMessagesFailed(int incr) {
+		this.numMessagesFailed += incr;
+	}
 
 }
