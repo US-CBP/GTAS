d001e1a6ef80599d342912256373295fb765d5df
jtangGitHub <jtangGitHub@gmail.com>
2016-08-09 16:49:40 -0400

    PassengerTimeline - integration test

--- gtas-parent/gtas-commons/src/test/java/gov/gtas/services/AuditLogPersistenceServiceIT.java
+++ gtas-parent/gtas-commons/src/test/java/gov/gtas/services/AuditLogPersistenceServiceIT.java
@@ -15,6 +15,7 @@ import gov.gtas.enumtype.Status;
 import gov.gtas.json.AuditActionData;
 import gov.gtas.json.AuditActionTarget;
 import gov.gtas.model.AuditRecord;
+import gov.gtas.model.Passenger;
 import gov.gtas.model.User;
 import gov.gtas.services.security.UserService;
 import gov.gtas.services.security.UserServiceUtil;
@@ -39,229 +40,254 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { CommonServicesConfig.class,
-        CachingConfig.class })
+		CachingConfig.class })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
 public class AuditLogPersistenceServiceIT {
 
-    @Autowired
-    private AuditLogPersistenceService testTarget;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserServiceUtil userServiceUtil;
+	@Autowired
+	private AuditLogPersistenceService testTarget;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserServiceUtil userServiceUtil;
 
-    @Before
-    public void setUp() throws Exception {
-    }
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@After
+	public void tearDown() throws Exception {
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLog() {
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
-                "UDR_TITLE", user));
-        List<AuditRecord> recList = testTarget.findByUser("jpjones");
-        assertNotNull(recList);
-        assertEquals(1, recList.size());
-        AuditRecord rec = recList.get(0);
-        assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
-        assertEquals(Status.SUCCESS, rec.getActionStatus());
-        assertEquals("UDR_TITLE", rec.getTarget());
-        assertNull(rec.getActionData());
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLog() {
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
+				"UDR_TITLE", user));
+		List<AuditRecord> recList = testTarget.findByUser("jpjones");
+		assertNotNull(recList);
+		assertEquals(1, recList.size());
+		AuditRecord rec = recList.get(0);
+		assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
+		assertEquals(Status.SUCCESS, rec.getActionStatus());
+		assertEquals("UDR_TITLE", rec.getTarget());
+		assertNull(rec.getActionData());
 
-    }
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLogWithData() {
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
-                "UDR_TITLE", Status.SUCCESS, "Creating UDR",
-                "{jhhgjghgkjhgkjh}", user));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
-                Status.SUCCESS_WITH_WARNING, "Creating WL", null, user));
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLogWithData() {
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
+				"UDR_TITLE", Status.SUCCESS, "Creating UDR",
+				"{jhhgjghgkjhgkjh}", user));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
+				Status.SUCCESS_WITH_WARNING, "Creating WL", null, user));
 
-        List<AuditRecord> recList = testTarget.findByUser("jpjones");
-        assertNotNull(recList);
-        assertEquals(2, recList.size());
+		List<AuditRecord> recList = testTarget.findByUser("jpjones");
+		assertNotNull(recList);
+		assertEquals(2, recList.size());
 
-        recList = testTarget.findByUserAndActionType(
-                AuditActionType.CREATE_UDR, "jpjones");
-        assertEquals(1, recList.size());
-        AuditRecord rec = recList.get(0);
-        assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
-        assertEquals(Status.SUCCESS, rec.getActionStatus());
-        assertEquals("UDR_TITLE", rec.getTarget());
-        assertEquals("Creating UDR", rec.getMessage());
-        assertEquals("{jhhgjghgkjhgkjh}", rec.getActionData());
+		recList = testTarget.findByUserAndActionType(
+				AuditActionType.CREATE_UDR, "jpjones");
+		assertEquals(1, recList.size());
+		AuditRecord rec = recList.get(0);
+		assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
+		assertEquals(Status.SUCCESS, rec.getActionStatus());
+		assertEquals("UDR_TITLE", rec.getTarget());
+		assertEquals("Creating UDR", rec.getMessage());
+		assertEquals("{jhhgjghgkjhgkjh}", rec.getActionData());
 
-        recList = testTarget.findByUserAndActionType(AuditActionType.CREATE_WL,
-                "jpjones");
-        assertEquals(1, recList.size());
-        rec = recList.get(0);
-        assertEquals(AuditActionType.CREATE_WL, rec.getActionType());
-        assertEquals("Creating WL", rec.getMessage());
-        assertEquals(Status.SUCCESS_WITH_WARNING, rec.getActionStatus());
-        assertEquals("WL_NAME", rec.getTarget());
-        assertNull(rec.getActionData());
-    }
+		recList = testTarget.findByUserAndActionType(AuditActionType.CREATE_WL,
+				"jpjones");
+		assertEquals(1, recList.size());
+		rec = recList.get(0);
+		assertEquals(AuditActionType.CREATE_WL, rec.getActionType());
+		assertEquals("Creating WL", rec.getMessage());
+		assertEquals(Status.SUCCESS_WITH_WARNING, rec.getActionStatus());
+		assertEquals("WL_NAME", rec.getTarget());
+		assertNull(rec.getActionData());
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLogWithDataObjects() throws Exception {
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        AuditActionTarget target = new AuditActionTarget(
-                AuditActionType.CREATE_UDR, "UDR_TITLE", null);
-        AuditActionData data = new AuditActionData();
-        data.addProperty("foo", "bar");
-        data.addProperty("foo2", "bar2");
-        testTarget.create(AuditActionType.CREATE_UDR, target, data,
-                "Creating UDR", user.getUserId());
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLogWithDataObjects() throws Exception {
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		AuditActionTarget target = new AuditActionTarget(
+				AuditActionType.CREATE_UDR, "UDR_TITLE", null);
+		AuditActionData data = new AuditActionData();
+		data.addProperty("foo", "bar");
+		data.addProperty("foo2", "bar2");
+		testTarget.create(AuditActionType.CREATE_UDR, target, data,
+				"Creating UDR", user.getUserId());
 
-        List<AuditRecord> recList = testTarget.findByUser("jpjones");
-        assertNotNull(recList);
-        assertEquals(1, recList.size());
+		List<AuditRecord> recList = testTarget.findByUser("jpjones");
+		assertNotNull(recList);
+		assertEquals(1, recList.size());
 
-        recList = testTarget.findByUserAndActionType(
-                AuditActionType.CREATE_UDR, "jpjones");
-        assertEquals(1, recList.size());
-        AuditRecord rec = recList.get(0);
-        assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
-        assertEquals(Status.SUCCESS, rec.getActionStatus());
-        assertEquals("{\"type\":\"UDR\",\"name\":\"UDR_TITLE\",\"id\":\"\"}",
-                rec.getTarget());
-        assertEquals("Creating UDR", rec.getMessage());
-        String actionData = rec.getActionData();
-        assertNotNull(actionData);
-        assertEquals(
-                "[{\"name\":\"foo\",\"value\":\"bar\"},{\"name\":\"foo2\",\"value\":\"bar2\"}]",
-                actionData);
-    }
+		recList = testTarget.findByUserAndActionType(
+				AuditActionType.CREATE_UDR, "jpjones");
+		assertEquals(1, recList.size());
+		AuditRecord rec = recList.get(0);
+		assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
+		assertEquals(Status.SUCCESS, rec.getActionStatus());
+		assertEquals("{\"type\":\"UDR\",\"name\":\"UDR_TITLE\",\"id\":\"\"}",
+				rec.getTarget());
+		assertEquals("Creating UDR", rec.getMessage());
+		String actionData = rec.getActionData();
+		assertNotNull(actionData);
+		assertEquals(
+				"[{\"name\":\"foo\",\"value\":\"bar\"},{\"name\":\"foo2\",\"value\":\"bar2\"}]",
+				actionData);
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLogWithQueryByActionType() {
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
-                "UDR_TITLE", user));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
-                Status.SUCCESS_WITH_WARNING, null, null, user));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
-                "WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
-                "WL_NAME3", Status.SUCCESS_WITH_WARNING, null, null, user));
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLogWithQueryByActionType() {
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
+				"UDR_TITLE", user));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
+				Status.SUCCESS_WITH_WARNING, null, null, user));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
+				"WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
+				"WL_NAME3", Status.SUCCESS_WITH_WARNING, null, null, user));
 
-        List<AuditRecord> recList = testTarget.findByUser("jpjones");
-        assertNotNull(recList);
-        assertEquals(4, recList.size());
+		List<AuditRecord> recList = testTarget.findByUser("jpjones");
+		assertNotNull(recList);
+		assertEquals(4, recList.size());
 
-        recList = testTarget.findByUserAndActionType(
-                AuditActionType.CREATE_UDR, "jpjones");
-        assertEquals(1, recList.size());
-        AuditRecord rec = recList.get(0);
-        assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
-        assertEquals(Status.SUCCESS, rec.getActionStatus());
-        assertEquals("UDR_TITLE", rec.getTarget());
-        assertNull(rec.getActionData());
+		recList = testTarget.findByUserAndActionType(
+				AuditActionType.CREATE_UDR, "jpjones");
+		assertEquals(1, recList.size());
+		AuditRecord rec = recList.get(0);
+		assertEquals(AuditActionType.CREATE_UDR, rec.getActionType());
+		assertEquals(Status.SUCCESS, rec.getActionStatus());
+		assertEquals("UDR_TITLE", rec.getTarget());
+		assertNull(rec.getActionData());
 
-        recList = testTarget.findByUserAndActionType(AuditActionType.CREATE_WL,
-                "jpjones");
-        assertEquals(3, recList.size());
-    }
+		recList = testTarget.findByUserAndActionType(AuditActionType.CREATE_WL,
+				"jpjones");
+		assertEquals(3, recList.size());
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLogByDate() {
-        TestUtils.insertAdminUser(userService, "nimitz", "password",
-                "firstName", "lastName");
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        User user2 = TestUtils
-                .fetchUser(userService, userServiceUtil, "nimitz");
-        Date date1 = new Date();
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-        }
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
-                "UDR_TITLE", user2));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
-                Status.SUCCESS_WITH_WARNING, null, null, user2));
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-        }
-        Date date2 = new Date();
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-        }
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
-                "WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
-                "WL_NAME3", Status.SUCCESS_WITH_WARNING, null, null, user));
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLogByDate() {
+		TestUtils.insertAdminUser(userService, "nimitz", "password",
+				"firstName", "lastName");
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		User user2 = TestUtils
+				.fetchUser(userService, userServiceUtil, "nimitz");
+		Date date1 = new Date();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
+				"UDR_TITLE", user2));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
+				Status.SUCCESS_WITH_WARNING, null, null, user2));
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		Date date2 = new Date();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
+				"WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
+				"WL_NAME3", Status.SUCCESS_WITH_WARNING, null, null, user));
 
-        List<AuditRecord> recList = testTarget.findByDateFrom(date1);
-        assertNotNull(recList);
-        assertEquals(4, recList.size());
+		List<AuditRecord> recList = testTarget.findByDateFrom(date1);
+		assertNotNull(recList);
+		assertEquals(4, recList.size());
 
-        recList = testTarget.findByDateRange(date1, date2);
-        assertEquals(2, recList.size());
-        AuditRecord rec = recList.get(0);
-        assertEquals("nimitz", rec.getUser().getUserId());
-    }
+		recList = testTarget.findByDateRange(date1, date2);
+		assertEquals(2, recList.size());
+		AuditRecord rec = recList.get(0);
+		assertEquals("nimitz", rec.getUser().getUserId());
+	}
 
-    @Transactional
-    @Test()
-    public void testCreateFetchAuditLogByTarget() {
-        TestUtils.insertAdminUser(userService, "nimitz", "password",
-                "firstName", "lastName");
-        TestUtils.insertAdminUser(userService, "jpjones", "password",
-                "firstName", "lastName");
-        User user = TestUtils
-                .fetchUser(userService, userServiceUtil, "jpjones");
-        User user2 = TestUtils
-                .fetchUser(userService, userServiceUtil, "nimitz");
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
-                "UDR_TITLE", user2));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
-                Status.SUCCESS_WITH_WARNING, "{DocWatchList1}", null, user2));
-        testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
-                "WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
-        testTarget.create(new AuditRecord(AuditActionType.UPDATE_WL, "WL_NAME",
-                Status.SUCCESS_WITH_WARNING, "{DocWatchList2}", null, user));
+	@Transactional
+	@Test()
+	public void testCreateFetchAuditLogByTarget() {
+		TestUtils.insertAdminUser(userService, "nimitz", "password",
+				"firstName", "lastName");
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+		User user2 = TestUtils
+				.fetchUser(userService, userServiceUtil, "nimitz");
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_UDR,
+				"UDR_TITLE", user2));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL, "WL_NAME",
+				Status.SUCCESS_WITH_WARNING, "{DocWatchList1}", null, user2));
+		testTarget.create(new AuditRecord(AuditActionType.CREATE_WL,
+				"WL_NAME2", Status.SUCCESS_WITH_WARNING, null, null, user));
+		testTarget.create(new AuditRecord(AuditActionType.UPDATE_WL, "WL_NAME",
+				Status.SUCCESS_WITH_WARNING, "{DocWatchList2}", null, user));
 
-        List<AuditRecord> recList = testTarget.findByUserAndTarget("nimitz",
-                "WL_NAME");
-        assertNotNull(recList);
-        assertEquals(1, recList.size());
-        assertEquals("{DocWatchList1}", recList.get(0).getMessage());
+		List<AuditRecord> recList = testTarget.findByUserAndTarget("nimitz",
+				"WL_NAME");
+		assertNotNull(recList);
+		assertEquals(1, recList.size());
+		assertEquals("{DocWatchList1}", recList.get(0).getMessage());
 
-        recList = testTarget.findByUserAndTarget("jpjones", "WL_NAME");
-        assertNotNull(recList);
-        assertEquals(1, recList.size());
-        assertEquals("{DocWatchList2}", recList.get(0).getMessage());
+		recList = testTarget.findByUserAndTarget("jpjones", "WL_NAME");
+		assertNotNull(recList);
+		assertEquals(1, recList.size());
+		assertEquals("{DocWatchList2}", recList.get(0).getMessage());
 
-        recList = testTarget.findByTarget("WL_NAME");
-        assertEquals(2, recList.size());
-    }
+		recList = testTarget.findByTarget("WL_NAME");
+		assertEquals(2, recList.size());
+	}
+
+	@Transactional
+	@Test()
+	public void testCreateFetchPassengerAuditLogByTarget() {
+		TestUtils.insertAdminUser(userService, "jpjones", "password",
+				"firstName", "lastName");
+		User user = TestUtils
+				.fetchUser(userService, userServiceUtil, "jpjones");
+
+		Passenger testPassenger = new Passenger();
+		testPassenger.setId(99990L);
+
+		AuditActionTarget target = new AuditActionTarget(testPassenger);
+		AuditActionData actionData = new AuditActionData();
+		actionData.addProperty("testProperty",
+				String.valueOf(testPassenger.getId()));
+		String message = "Message Loader run on " + new Date();
+		testTarget.create(AuditActionType.RULE_HIT, target, actionData,
+				message, user.getUserId());
+
+		List<AuditRecord> recList = testTarget.findByTarget(target.toString());
+		assertEquals(1, recList.size());
+		assertEquals(recList.get(0).getTarget(), target.toString());
+
+	}
 
 }
