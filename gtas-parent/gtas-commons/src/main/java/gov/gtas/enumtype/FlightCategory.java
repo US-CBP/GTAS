f969845bbd2d891f8ae76b8a557a953d7ed2cf45
jtangGitHub <jtangGitHub@gmail.com>
2016-09-16 09:45:32 -0400

    for International/domestic filter

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/enumtype/FlightCategory.java
@@ -0,0 +1,20 @@
+/*
+* All GTAS code is Copyright 2016, Unisys Corporation.
+* 
+* Please see LICENSE.txt for details.
+*/
+
+package gov.gtas.enumtype;
+
+/**
+ * The Enum FlightCategory.
+ */
+public enum FlightCategory {	    
+    	/** International */
+    	I,	    
+    	/** Domestic */
+    	D,	    
+    	/** All */
+    	A
+}
+
