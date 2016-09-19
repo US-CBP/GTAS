914bbdd0bb1e4a58d8e85b1027c6d2293a692f17
jtangGitHub <jtangGitHub@gmail.com>
2016-09-19 14:01:09 -0400

    code for interational/domestic filter

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/constant/FlightCategoryConstants.java
@@ -0,0 +1,17 @@
+/*
+ * All GTAS code is Copyright 2016, Unisys Corporation.
+ * 
+ * Please see LICENSE.txt for details.
+ */
+package gov.gtas.constant;
+
+/**
+ * The Class FlightCategoryConstants.
+ */
+public class FlightCategoryConstants {
+	// Flight Category
+	public static final String INTERNATIONAL_FLIGHTS = "International";
+	public static final String DOMESTIC_FLIGHTS = "Domestic";
+	public static final String ALL_FLIGHTS = "All";
+
+}
