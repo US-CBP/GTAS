72ebd33ffd9a684e30414895457ae5857edf3d12
jtangGitHub <jtangGitHub@gmail.com>
2016-10-06 15:53:53 -0400

    update whitelist code

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/model/Whitelist.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/model/Whitelist.java
@@ -73,4 +73,227 @@ public class Whitelist extends BaseEntityAudit {
 	@JoinColumn(name = "editor", referencedColumnName = "user_id", nullable = false)
 	private User whiteListEditor;
 
+	public YesNoEnum getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(YesNoEnum deleted) {
+		this.deleted = deleted;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getCitizenshipCountry() {
+		return citizenshipCountry;
+	}
+
+	public void setCitizenshipCountry(String citizenshipCountry) {
+		this.citizenshipCountry = citizenshipCountry;
+	}
+
+	public String getResidencyCountry() {
+		return residencyCountry;
+	}
+
+	public void setResidencyCountry(String residencyCountry) {
+		this.residencyCountry = residencyCountry;
+	}
+
+	public String getDocumentType() {
+		return documentType;
+	}
+
+	public void setDocumentType(String documentType) {
+		this.documentType = documentType;
+	}
+
+	public String getDocumentNumber() {
+		return documentNumber;
+	}
+
+	public void setDocumentNumber(String documentNumber) {
+		this.documentNumber = documentNumber;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public Date getIssuanceDate() {
+		return issuanceDate;
+	}
+
+	public void setIssuanceDate(Date issuanceDate) {
+		this.issuanceDate = issuanceDate;
+	}
+
+	public String getIssuanceCountry() {
+		return issuanceCountry;
+	}
+
+	public void setIssuanceCountry(String issuanceCountry) {
+		this.issuanceCountry = issuanceCountry;
+	}
+
+	public User getWhiteListEditor() {
+		return whiteListEditor;
+	}
+
+	public void setWhiteListEditor(User whiteListEditor) {
+		this.whiteListEditor = whiteListEditor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime
+				* result
+				+ ((citizenshipCountry == null) ? 0 : citizenshipCountry
+						.hashCode());
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result
+				+ ((documentNumber == null) ? 0 : documentNumber.hashCode());
+		result = prime * result
+				+ ((documentType == null) ? 0 : documentType.hashCode());
+		result = prime * result
+				+ ((expirationDate == null) ? 0 : expirationDate.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result
+				+ ((issuanceCountry == null) ? 0 : issuanceCountry.hashCode());
+		result = prime * result
+				+ ((issuanceDate == null) ? 0 : issuanceDate.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+				+ ((middleName == null) ? 0 : middleName.hashCode());
+		result = prime
+				* result
+				+ ((residencyCountry == null) ? 0 : residencyCountry.hashCode());
+		result = prime * result
+				+ ((whiteListEditor == null) ? 0 : whiteListEditor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Whitelist other = (Whitelist) obj;
+		if (citizenshipCountry == null) {
+			if (other.citizenshipCountry != null)
+				return false;
+		} else if (!citizenshipCountry.equals(other.citizenshipCountry))
+			return false;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (documentNumber == null) {
+			if (other.documentNumber != null)
+				return false;
+		} else if (!documentNumber.equals(other.documentNumber))
+			return false;
+		if (documentType == null) {
+			if (other.documentType != null)
+				return false;
+		} else if (!documentType.equals(other.documentType))
+			return false;
+		if (expirationDate == null) {
+			if (other.expirationDate != null)
+				return false;
+		} else if (!expirationDate.equals(other.expirationDate))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (issuanceCountry == null) {
+			if (other.issuanceCountry != null)
+				return false;
+		} else if (!issuanceCountry.equals(other.issuanceCountry))
+			return false;
+		if (issuanceDate == null) {
+			if (other.issuanceDate != null)
+				return false;
+		} else if (!issuanceDate.equals(other.issuanceDate))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (middleName == null) {
+			if (other.middleName != null)
+				return false;
+		} else if (!middleName.equals(other.middleName))
+			return false;
+		if (residencyCountry == null) {
+			if (other.residencyCountry != null)
+				return false;
+		} else if (!residencyCountry.equals(other.residencyCountry))
+			return false;
+		if (whiteListEditor == null) {
+			if (other.whiteListEditor != null)
+				return false;
+		} else if (!whiteListEditor.equals(other.whiteListEditor))
+			return false;
+		return true;
+	}
+
 }
--- gtas-parent/gtas-commons/src/main/java/gov/gtas/services/WhitelistServiceImpl.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/services/WhitelistServiceImpl.java
@@ -5,37 +5,90 @@
  */
 package gov.gtas.services;
 
+import gov.gtas.enumtype.YesNoEnum;
 import gov.gtas.model.Whitelist;
+import gov.gtas.repository.WhitelistRepository;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WhitelistServiceImpl implements WhitelistService {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(WhitelistServiceImpl.class);
+
+	@Resource
+	private WhitelistRepository whitelistRepository;
+
 	@Override
+	@Transactional
 	public List<Whitelist> getAllWhitelists() {
-		// TODO Auto-generated method stub
-		return null;
+		return whitelistRepository.findAllbydeleted(YesNoEnum.N);
 	}
 
 	@Override
+	@Transactional
 	public Whitelist delete(Long id, String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		Whitelist wl = whitelistRepository.findOne(id);
+		if (wl != null) {
+			whitelistRepository.delete(wl);
+		} else {
+			logger.warn("The id = " + id
+					+ " of whitelist object does not exist.");
+		}
+		return wl;
 	}
 
 	@Override
-	public Whitelist create(Whitelist rule, String userId) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public Whitelist create(Whitelist wl, String userId) {
+		return whitelistRepository.save(wl);
 	}
 
 	@Override
-	public Whitelist update(Whitelist whitelist, String userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	@Transactional
+	public Whitelist update(Whitelist wl, String userId) {
+		Whitelist updatedWl = null;
+		Whitelist existing = whitelistRepository.findOne(wl.getId());
+		if (existing != null) {
+			if (wl.getCitizenshipCountry() != null)
+				existing.setCitizenshipCountry(wl.getCitizenshipCountry());
+			if (wl.getDeleted() != null)
+				existing.setDeleted(wl.getDeleted());
+			if (wl.getDeleted() != null)
+				existing.setDob(wl.getDob());
+			if (wl.getDocumentNumber() != null)
+				existing.setDocumentNumber(wl.getDocumentNumber());
+			if (wl.getDocumentType() != null)
+				existing.setDocumentType(wl.getDocumentType());
+			if (wl.getExpirationDate() != null)
+				existing.setExpirationDate(wl.getExpirationDate());
+			if (wl.getFirstName() != null)
+				existing.setFirstName(wl.getFirstName());
+			if (wl.getGender() != null)
+				existing.setGender(wl.getGender());
+			if (wl.getIssuanceCountry() != null)
+				existing.setIssuanceCountry(wl.getIssuanceCountry());
+			if (wl.getCitizenshipCountry() != null)
+				existing.setIssuanceDate(wl.getIssuanceDate());
+			if (wl.getIssuanceDate() != null)
+				existing.setLastName(wl.getLastName());
+			if (wl.getMiddleName() != null)
+				existing.setMiddleName(wl.getMiddleName());
+			if (wl.getResidencyCountry() != null)
+				existing.setResidencyCountry(wl.getResidencyCountry());
+			if (wl.getWhiteListEditor() != null)
+				existing.setWhiteListEditor(wl.getWhiteListEditor());
 
+			updatedWl = whitelistRepository.save(existing);
+		}
+		return updatedWl;
+	}
 }
