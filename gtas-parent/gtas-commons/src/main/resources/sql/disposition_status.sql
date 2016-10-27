1e2467b7f017a22122e7fbacfa0c2e4a16833727
jtangGitHub <jtangGitHub@gmail.com>
2016-10-27 18:44:31 -0400

    updated sql error

--- gtas-parent/gtas-commons/src/main/resources/sql/disposition_status.sql
+++ gtas-parent/gtas-commons/src/main/resources/sql/disposition_status.sql
@@ -1,6 +1,6 @@
 /*These 4 statuses are irremovable (though mutable) and must exist in some form in order to preserve the case management flow, with this order for ID purposes. */
-insert into disposition_status(name, description) values('NEW', 'New Case')
+insert into disposition_status(name, description) values('NEW', 'New Case');
 insert into disposition_status(name, description) values('OPEN', 'Case is open');
 insert into disposition_status(name, description) values('CLOSED', 'No action required');
-insert into disposition_status(name, description) values('RE-OPEN'), 'Re-opened case');
+insert into disposition_status(name, description) values('RE-OPEN', 'Re-opened case');
 insert into disposition_status(name, description) values('PENDING CLOSURE','Case is pending closure');
