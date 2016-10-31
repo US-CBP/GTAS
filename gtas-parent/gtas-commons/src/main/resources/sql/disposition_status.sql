3bbdd04d4909fdfea4c84dc32b6b86a55d435f71
Jon <Jon@Jon-PC>
2016-10-31 16:08:08 -0400

    Change to disp-status sql
    -Change to insert statements for disposition statuses. Mandating
    particular ids required for the case flow.

--- gtas-parent/gtas-commons/src/main/resources/sql/disposition_status.sql
+++ gtas-parent/gtas-commons/src/main/resources/sql/disposition_status.sql
@@ -1,6 +1,6 @@
 /*These 4 statuses are irremovable (though mutable) and must exist in some form in order to preserve the case management flow, with this order for ID purposes. */
-insert into disposition_status(name, description) values('NEW', 'New Case');
-insert into disposition_status(name, description) values('OPEN', 'Case is open');
-insert into disposition_status(name, description) values('CLOSED', 'No action required');
-insert into disposition_status(name, description) values('RE-OPEN', 'Re-opened case');
-insert into disposition_status(name, description) values('PENDING CLOSURE','Case is pending closure');
+insert into disposition_status(id, name, description) values(1, 'NEW', 'New Case');
+insert into disposition_status(id, name, description) values(2, 'OPEN', 'Case is open');
+insert into disposition_status(id, name, description) values(3, 'CLOSED', 'No action required');
+insert into disposition_status(id, name, description) values(4, 'RE-OPEN', 'Re-opened case');
+insert into disposition_status(id, name, description) values(5, 'PENDING CLOSURE','Case is pending closure');
