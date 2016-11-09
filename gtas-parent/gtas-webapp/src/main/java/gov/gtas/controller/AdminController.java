f981eb6ade6e9ffd8ed05ee1c8c29f6e18a7bcc4
jtangGitHub <jtangGitHub@gmail.com>
2016-11-09 16:25:31 -0500

    update code and remove error

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/AdminController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/AdminController.java
@@ -23,6 +23,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,106 +37,119 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(AdminController.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(AdminController.class);
 
-    @Autowired
-    private AuditLogPersistenceService auditService;
-    
-    @Autowired
-    private ErrorPersistenceService errorService;
+	@Autowired
+	private AuditLogPersistenceService auditService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/auditlog")
-    public List<AuditRecordVo> getAuditlog(
-            @RequestParam(value="user", required=false) String user,
-            @RequestParam(value="action", required=false) String action,
-            @RequestParam(value="startDate", required=false) String startDate,
-            @RequestParam(value="endDate", required=false) String endDate) throws ParseException{
-        Date st = null;
-        Date nd = null;
-        if(startDate != null){
-            st = DateCalendarUtils.parseJsonDate(startDate);
-        }
-        if(endDate != null){
-            nd = DateCalendarUtils.parseJsonDate(endDate);
-            nd = DateCalendarUtils.addOneDayToDate(nd);
-        }
-        AuditActionType actionType = null;
-        if(action != null && !action.equals(AuditLogConstants.SHOW_ALL_ACTION)){
-            try{
-                actionType = AuditActionType.valueOf(action);
-            } catch (Exception ex){
-                logger.error("AdminController.getAuditlog - invalid action type:"+action);
-            }
-        }
-        return fetchAuditLogData(user, actionType, st, nd);
-    }
-    
-    @RequestMapping(method = RequestMethod.GET, value = "/errorlog")
-    public List<ErrorDetailInfo> getErrorlog(
-            @RequestParam(value="code", required=false) String code,
-            @RequestParam(value="startDate", required=false) String startDate,
-            @RequestParam(value="endDate", required=false) String endDate) throws ParseException{
-        Date st = null;
-        Date nd = null;
-        if(startDate != null){
-            st = DateCalendarUtils.parseJsonDate(startDate);
-        }
-        if(endDate != null){
-            nd = DateCalendarUtils.parseJsonDate(endDate);
-            nd = DateCalendarUtils.addOneDayToDate(nd);
-        }
-        return fetchErrorLogData(code, st, nd);
-    }
+	@Autowired
+	private ErrorPersistenceService errorService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/auditlog/{startDate}/{endDate}")
-    public List<AuditRecordVo> getAuditlog(@PathVariable String startDate, @PathVariable String endDate) throws ParseException {
-        Date stdt = StringUtils.isEmpty(startDate) ? null:DateCalendarUtils.parseJsonDate(startDate);
-        Date endt = StringUtils.isEmpty(endDate) ? null:DateCalendarUtils.parseJsonDate(endDate);
-        return fetchAuditLogData(stdt, endt);
-    }
-    private List<AuditRecordVo> fetchAuditLogData(Date startDate, Date endDate) {
-        return fetchAuditLogData(null, null, startDate, endDate);
-    }
-    private List<AuditRecordVo> fetchAuditLogData(String userId, AuditActionType action, Date startDate, Date endDate) {
-        Date from = startDate;
-        Date to = endDate;
-        if (from == null && StringUtils.isEmpty(userId) && action == null) {
-            logger.debug("AdminController: fetchAuditLogData - start date is null, using current date.");
-            from = DateCalendarUtils.stripTime(new Date());
-        }
+	@RequestMapping(method = RequestMethod.GET, value = "/auditlog")
+	public List<AuditRecordVo> getAuditlog(
+			@RequestParam(value = "user", required = false) String user,
+			@RequestParam(value = "action", required = false) String action,
+			@RequestParam(value = "startDate", required = false) String startDate,
+			@RequestParam(value = "endDate", required = false) String endDate)
+			throws ParseException {
+		Date st = null;
+		Date nd = null;
+		if (startDate != null) {
+			st = DateCalendarUtils.parseJsonDate(startDate);
+		}
+		if (endDate != null) {
+			nd = DateCalendarUtils.parseJsonDate(endDate);
+			nd = DateCalendarUtils.addOneDayToDate(nd);
+		}
+		AuditActionType actionType = null;
+		if (action != null && !action.equals(AuditLogConstants.SHOW_ALL_ACTION)) {
+			try {
+				actionType = AuditActionType.valueOf(action);
+			} catch (Exception ex) {
+				logger.error("AdminController.getAuditlog - invalid action type:"
+						+ action);
+			}
+		}
+		return fetchAuditLogData(user, actionType, st, nd);
+	}
 
-        if (from != null && to == null) {
-            logger.debug("AdminController: fetchAuditLogData - end date is null, using current date.");
-            to = DateCalendarUtils.addOneDayToDate(DateCalendarUtils.stripTime(new Date()));
-        }
-        List<AuditRecord> res = auditService.findByUserActionDateRange(userId, action, from, to);
-        List<AuditRecordVo> ret = new LinkedList<AuditRecordVo>();
-        if (res != null) {
-            res.forEach(ar -> ret.add(new AuditRecordVo(ar)));
-        }
-        return ret;
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "/errorlog")
+	public List<ErrorDetailInfo> getErrorlog(
+			@RequestParam(value = "code", required = false) String code,
+			@RequestParam(value = "startDate", required = false) String startDate,
+			@RequestParam(value = "endDate", required = false) String endDate)
+			throws ParseException {
+		Date st = null;
+		Date nd = null;
+		if (startDate != null) {
+			st = DateCalendarUtils.parseJsonDate(startDate);
+		}
+		if (endDate != null) {
+			nd = DateCalendarUtils.parseJsonDate(endDate);
+			nd = DateCalendarUtils.addOneDayToDate(nd);
+		}
+		return fetchErrorLogData(code, st, nd);
+	}
 
-    private List<ErrorDetailInfo> fetchErrorLogData(String code, Date startDate, Date endDate) {
-        Date from = startDate;
-        Date to = endDate;
-        if (from == null && StringUtils.isEmpty(code)) {
-            logger.debug("AdminController: fetchErrorLogData - start date is null, using current date.");
-            from = DateCalendarUtils.stripTime(new Date());
-        }
+	@RequestMapping(method = RequestMethod.GET, value = "/auditlog/{startDate}/{endDate}")
+	public List<AuditRecordVo> getAuditlog(@PathVariable String startDate,
+			@PathVariable String endDate) throws ParseException {
+		Date stdt = StringUtils.isEmpty(startDate) ? null : DateCalendarUtils
+				.parseJsonDate(startDate);
+		Date endt = StringUtils.isEmpty(endDate) ? null : DateCalendarUtils
+				.parseJsonDate(endDate);
+		return fetchAuditLogData(stdt, endt);
+	}
 
-        if (from != null && to == null) {
-            logger.debug("AdminController: fetchErrorLogData - end date is null, using current date.");
-            to = DateCalendarUtils.addOneDayToDate(DateCalendarUtils.stripTime(new Date()));
-        }
-        List<ErrorDetailInfo> res = null;
-        if(StringUtils.isEmpty(code)){
-         res = errorService.findByDateRange(from, to);
-        } else {
-            res = errorService.findByCode(code);
-        }
-        return res;
-    }
+	private List<AuditRecordVo> fetchAuditLogData(Date startDate, Date endDate) {
+		return fetchAuditLogData(null, null, startDate, endDate);
+	}
+
+	private List<AuditRecordVo> fetchAuditLogData(String userId,
+			AuditActionType action, Date startDate, Date endDate) {
+		Date from = startDate;
+		Date to = endDate;
+		if (from == null && StringUtils.isEmpty(userId) && action == null) {
+			logger.debug("AdminController: fetchAuditLogData - start date is null, using current date.");
+			from = DateCalendarUtils.stripTime(new Date());
+		}
+
+		if (from != null && to == null) {
+			logger.debug("AdminController: fetchAuditLogData - end date is null, using current date.");
+			to = DateCalendarUtils.addOneDayToDate(DateCalendarUtils
+					.stripTime(new Date()));
+		}
+		List<AuditRecord> res = auditService.findByUserActionDateRange(userId,
+				action, from, to);
+		List<AuditRecordVo> ret = new LinkedList<AuditRecordVo>();
+		if (res != null) {
+			res.forEach(ar -> ret.add(new AuditRecordVo(ar)));
+		}
+		return ret;
+	}
+
+	private List<ErrorDetailInfo> fetchErrorLogData(String code,
+			Date startDate, Date endDate) {
+		Date from = startDate;
+		Date to = endDate;
+		if (from == null && StringUtils.isEmpty(code)) {
+			logger.debug("AdminController: fetchErrorLogData - start date is null, using current date.");
+			from = DateCalendarUtils.stripTime(new Date());
+		}
+
+		if (from != null && to == null) {
+			logger.debug("AdminController: fetchErrorLogData - end date is null, using current date.");
+			to = DateCalendarUtils.addOneDayToDate(DateCalendarUtils
+					.stripTime(new Date()));
+		}
+		List<ErrorDetailInfo> res = null;
+		if (StringUtils.isEmpty(code)) {
+			res = errorService.findByDateRange(from, to);
+		} else {
+			res = errorService.findByCode(code);
+		}
+		return res;
+	}
 
 }
--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/security/AppSecurityConfig.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/security/AppSecurityConfig.java
@@ -5,111 +5,117 @@
  */
 package gov.gtas.security;
 
+import javax.sql.DataSource;
 
-import gov.gtas.security.AjaxAuthenticationSuccessHandler;
-
-import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeaderBindingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import gov.gtas.security.filters.CsrfHeaderFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import gov.gtas.constants.Constants;
-import javax.sql.DataSource;
+
+import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeaderBindingFilter;
 
 /**
  *
- * The Spring Security configuration for the application - its a form login config with authentication via session cookie (once logged in),
- * with fallback to HTTP Basic for non-browser clients.
+ * The Spring Security configuration for the application - its a form login
+ * config with authentication via session cookie (once logged in), with fallback
+ * to HTTP Basic for non-browser clients.
  *
  * The CSRF token is put on the reply as a header via a filter.
  *
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AppSecurityConfig.class);
-    
-    @Autowired
-    private SecurityUserDetailsService userDetailsService;
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(AppSecurityConfig.class);
 
-    @Autowired
-    DataSource dataSource;
-    
-    public void configure(WebSecurity web) throws Exception {
-      web.ignoring().antMatchers("/factory/**/*","/admin/**/*","/flights/**/*","/pax/**/*","/query-builder/**/*","/watchlists/**/*",
-                                 "/build/**/*","/dashboard/**/*","/dist/**/*","/jqb/**/*","/userSettings/**/*","/cases/**/*",
-                                 "/resources/**","/common/**/*","/login/**","/admin/**","/app.js","WEB-INF/**/*","/data/**");
-    }
+	@Autowired
+	private SecurityUserDetailsService userDetailsService;
 
+	@Autowired
+	DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/factory/**/*", "/admin/**/*",
+				"/flights/**/*", "/pax/**/*", "/query-builder/**/*",
+				"/watchlists/**/*", "/build/**/*", "/dashboard/**/*",
+				"/dist/**/*", "/jqb/**/*", "/userSettings/**/*", "/cases/**/*",
+				"/resources/**", "/common/**/*", "/login/**", "/admin/**",
+				"/app.js", "WEB-INF/**/*", "/data/**");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(
+				new BCryptPasswordEncoder());
+	}
 
-        SavedRequestAwareAuthenticationSuccessHandler savedReqHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-        CsrfTokenResponseHeaderBindingFilter csrfTokenFilter = new CsrfTokenResponseHeaderBindingFilter();
-        http.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
-        .csrf().csrfTokenRepository(csrfTokenRepository());
-		
-		http
-                .csrf().disable();
+		SavedRequestAwareAuthenticationSuccessHandler savedReqHandler = new SavedRequestAwareAuthenticationSuccessHandler();
 
-        http
-            .authorizeRequests()
-            .antMatchers("/resources/*/**","/resources/**/*","/resources/**","/common/**","/login/**","/authenticate").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginProcessingUrl("/authenticate")
-            .usernameParameter("username")
-            .passwordParameter("password")
-            .successHandler(new AjaxAuthenticationSuccessHandler(savedReqHandler))
-            .failureHandler(new SimpleUrlAuthenticationFailureHandler())
-            .loginPage("/login.html")
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login.html")
-            .invalidateHttpSession(true)
-            .permitAll();
+		CsrfTokenResponseHeaderBindingFilter csrfTokenFilter = new CsrfTokenResponseHeaderBindingFilter();
+		http.addFilterAfter(csrfTokenFilter, CsrfFilter.class).csrf()
+				.csrfTokenRepository(csrfTokenRepository());
 
-            http.sessionManagement().maximumSessions(1).and().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-        
-        if ("true".equals(System.getProperty("httpsOnly"))) {
-            LOGGER.info("launching the application in HTTPS-only mode");
-            http.requiresChannel().anyRequest().requiresSecure();
-        }
-    }
-    
-    
-      /**
-      * Util Method to add XSRF Token into the HTTP Header
-      */
-     private CsrfTokenRepository csrfTokenRepository() {
-              HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-              repository.setHeaderName("X-CSRF-TOKEN");
-              return repository;
-        }
-    
+		http.csrf().disable();
+
+		http.authorizeRequests()
+				.antMatchers("/resources/*/**", "/resources/**/*",
+						"/resources/**", "/common/**", "/login/**",
+						"/authenticate")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin()
+				.loginProcessingUrl("/authenticate")
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.successHandler(
+						new AjaxAuthenticationSuccessHandler(savedReqHandler))
+				.failureHandler(new SimpleUrlAuthenticationFailureHandler())
+				.loginPage("/login.html")
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login.html")
+				.invalidateHttpSession(true)
+				.permitAll();
+
+		http.sessionManagement().maximumSessions(1).and()
+				.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+
+		if ("true".equals(System.getProperty("httpsOnly"))) {
+			LOGGER.info("launching the application in HTTPS-only mode");
+			http.requiresChannel().anyRequest().requiresSecure();
+		}
+	}
+
+	/**
+	 * Util Method to add XSRF Token into the HTTP Header
+	 */
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-CSRF-TOKEN");
+		return repository;
+	}
+
 }
