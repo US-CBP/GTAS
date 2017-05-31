080814f6fb15a2f6ba9f6ddd9ed4045a89841083
ML-Git <maneeshlekkala@gmail.com>
2017-05-31 09:10:35 -0500

    Code-share plus other recent feature additions

--- gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/RuleRunner.java
+++ gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/RuleRunner.java
@@ -5,6 +5,11 @@
  */
 package gov.gtas.rule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import gov.gtas.config.CommonServicesConfig;
 import gov.gtas.constant.RuleServiceConstants;
 import gov.gtas.error.ErrorDetailInfo;
@@ -12,19 +17,13 @@ import gov.gtas.error.ErrorHandlerFactory;
 import gov.gtas.services.ErrorPersistenceService;
 import gov.gtas.svc.TargetingService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 /**
  * A Java application for running the Rule Engine in stand alone mode.
  *
  */
 public class RuleRunner {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(RuleRunner.class);
+	private static final Logger logger = LoggerFactory.getLogger(RuleRunner.class);
 
 	private RuleRunner() {
 
@@ -38,22 +37,17 @@ public class RuleRunner {
 	 */
 	public static void main(String[] args) {
 		logger.info("Entering main().");
-		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(
-				CommonServicesConfig.class);
-		TargetingService targetingService = (TargetingService) ctx
-				.getBean("targetingServiceImpl");
-
+		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(CommonServicesConfig.class);
+		TargetingService targetingService = (TargetingService) ctx.getBean("targetingServiceImpl");
 		try {
 			targetingService.preProcessing();
 			targetingService.runningRuleEngine();
 			logger.info("Exiting main().");
 		} catch (Exception exception) {
 			logger.debug(exception.getMessage());
-			ErrorDetailInfo errInfo = ErrorHandlerFactory.createErrorDetails(
-					RuleServiceConstants.RULE_ENGINE_RUNNER_ERROR_CODE,
-					exception);
-			ErrorPersistenceService errorService = (ErrorPersistenceService) ctx
-					.getBean("errorPersistenceServiceImpl");
+			ErrorDetailInfo errInfo = ErrorHandlerFactory
+					.createErrorDetails(RuleServiceConstants.RULE_ENGINE_RUNNER_ERROR_CODE, exception);
+			ErrorPersistenceService errorService = (ErrorPersistenceService) ctx.getBean("errorPersistenceServiceImpl");
 			errorService.create(errInfo);
 		} finally {
 			ctx.close();
