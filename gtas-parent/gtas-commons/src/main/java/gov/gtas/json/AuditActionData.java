99c64b20af251d7fc8b522b37921e98cb0cd6f82
Michael Copenhafer <mikecope@gmail.com>
2017-06-22 12:12:32 -0400

    updated copyright

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/json/AuditActionData.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/json/AuditActionData.java
@@ -1,5 +1,5 @@
 /*
- * All GTAS code is Copyright 2016, Unisys Corporation.
+ * All GTAS code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
  * 
  * Please see LICENSE.txt for details.
  */
