6901bfa7d4156d532ddb59e43ee7f8d3bedd322d
ml-git <maneeshlekkala@gmail.com>
2017-10-26 14:06:37 -0400

    Attachment Upload for Case Mgmt.

--- gtas-parent/gtas-webapp/src/main/webapp/cases/CaseDispositionDetailController.js
+++ gtas-parent/gtas-webapp/src/main/webapp/cases/CaseDispositionDetailController.js
@@ -181,14 +181,69 @@
                         else if($scope.hitDetailTrueHitFlag == 'false') $scope.hitDetailTrueHitFlag = false;
                 }
                 $mdSidenav(id).toggle();
-            }
+            };
 
 
             //Angular Trix related event handlers
             $scope.trixInitialize = function(e, editor) {
                 angular.element(editor.element).prop('contenteditable', false);
                 angular.element(editor.element.toolbarElement).remove();
-            }
+            };
+
+            // Trix attachment logic
+
+            var createStorageKey, host, uploadAttachment;
+
+            $scope.trixAttachmentAdd = function(e) {
+                var attachment;
+                attachment = e.attachment;
+                if (attachment.file) {
+                    return uploadAttachment(attachment);
+                }
+            };
+
+            host = "//";
+
+            uploadAttachment = function(attachment) {
+                var file, form, key, xhr;
+                file = attachment.file;
+                key = createStorageKey(file);
+                form = new FormData;
+                form.append("key", key);
+                form.append("Content-Type", file.type);
+                form.append("file", file);
+                xhr = new XMLHttpRequest;
+                xhr.open("POST", host, true);
+                xhr.upload.onprogress = function(event) {
+                    var progress;
+                    progress = event.loaded / event.total * 100;
+                    return attachment.setUploadProgress(progress);
+                };
+                xhr.onload = function() {
+                    var href, url;
+                    if (xhr.status === 204) {
+                        url = href = host + key;
+                        return attachment.setAttributes({
+                            url: url,
+                            href: href
+                        });
+                    }
+                };
+                return xhr.send(form);
+            };
+
+            createStorageKey = function(file) {
+                var date, day, time;
+                date = new Date();
+                day = date.toISOString().slice(0, 10);
+                time = date.getTime();
+                return "tmp/" + day + "/" + time + "-" + file.name;
+            };
+
+
+            // End Trix attachment logic
+
+
 
         })
 }());
