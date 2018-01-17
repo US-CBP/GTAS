ae1ab1bfd4da8438d60f218428619e0ca7af7793
ml-git <maneeshlekkala@gmail.com>
2018-01-17 17:27:55 -0500

    more redisson additions

--- gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/redisson/model/LedgerLiveObject.java
+++ gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/redisson/model/LedgerLiveObject.java
@@ -8,10 +8,16 @@ package gov.gtas.parsers.redisson.model;
 import org.redisson.api.annotation.REntity;
 import org.redisson.api.annotation.RId;
 
+import java.util.Date;
+
 @REntity
 public class LedgerLiveObject {
     @RId
     private String name;
+    private String pnrTag;
+    private String messagePayload;
+    private Date messageTimeStamp;
+    private Date processedTimeStamp;
 
     public String getName() {
         return name;
@@ -19,5 +25,38 @@ public class LedgerLiveObject {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public String getPnrTag() {
+        return pnrTag;
+    }
+
+    public void setPnrTag(String pnrTag) {
+        this.pnrTag = pnrTag;
+    }
+
+    public String getMessagePayload() {
+        return messagePayload;
+    }
+
+    public void setMessagePayload(String messagePayload) {
+        this.messagePayload = messagePayload;
+    }
+
+    public Date getMessageTimeStamp() {
+        return messageTimeStamp;
+    }
+
+    public void setMessageTimeStamp(Date messageTimeStamp) {
+        this.messageTimeStamp = messageTimeStamp;
+    }
+
+    public Date getProcessedTimeStamp() {
+        return processedTimeStamp;
+    }
+
+    public void setProcessedTimeStamp(Date processedTimeStamp) {
+        this.processedTimeStamp = processedTimeStamp;
     }
 }
