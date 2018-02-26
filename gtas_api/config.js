b52d4bc6454d9801cfb437a80f0d7aef40887f08
Alex Donley <donleyac@gmail.com>
2018-02-26 09:42:20 -0500

    GTAS Api Module

---
+++ gtas_api/config.js
@@ -0,0 +1,9 @@
+export const jwtSecret = "CHANGEME";
+//In Seconds
+export const jwtExpire = 120*60;
+export const dbConfig = {
+  name: "gtas",
+  user: "root",
+  pass: "admin"
+};
+export const PORT = 3000;
