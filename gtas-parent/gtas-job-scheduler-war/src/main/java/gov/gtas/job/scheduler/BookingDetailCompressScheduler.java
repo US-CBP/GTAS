bf3d30e0d2917b2c67e7447bf0f8919cfde04962
ml-git <maneeshlekkala@gmail.com>
2018-03-28 11:04:13 -0400

    update travis settings

---
+++ gtas-parent/gtas-job-scheduler-war/src/main/java/gov/gtas/job/scheduler/BookingDetailCompressScheduler.java
@@ -0,0 +1,10 @@
+/*
+ * All GTAS code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+ *
+ * Please see LICENSE.txt for details.
+ */
+package gov.gtas.job.scheduler;
+
+public class BookingDetailCompressScheduler {
+
+}
