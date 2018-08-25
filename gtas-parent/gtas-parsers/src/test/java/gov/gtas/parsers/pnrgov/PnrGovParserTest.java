278e284b5fded338ff72beb64fe7bb8676576a37
Robert Stine <robwstine@outlook.com>
2018-08-25 18:52:03 -0400

    Updated unit test relating to booking date and reservation date. Added another test case from the spec.

--- gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/pnrgov/PnrGovParserTest.java
+++ gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/pnrgov/PnrGovParserTest.java
@@ -1,6 +1,6 @@
 /*
  * All GTAS code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
- * 
+ *
  * Please see LICENSE.txt for details.
  */
 package gov.gtas.parsers.pnrgov;
@@ -11,10 +11,14 @@ import org.junit.Test;
 import gov.gtas.parsers.edifact.EdifactParser;
 import gov.gtas.parsers.exception.ParseException;
 import gov.gtas.parsers.vo.PnrVo;
+
 import static org.junit.Assert.assertTrue;
+
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 
 public class PnrGovParserTest {
     private EdifactParser<PnrVo> parser;
@@ -23,31 +27,48 @@ public class PnrGovParserTest {
     public void setUp() {
         this.parser = new PnrGovParser();
     }
+
     /*
-    * The following test use a slightly modified message found on page 77 of the
-    * PASSENGER AND AIRPORT DATA
-    * INTERCHANGE STANDARDS EDIFACT IMPLEMENTATION GUIDE
-    * PNR DATA PUSHED TO STATES  OR OTHER AUTHORITIES
-    * PNRGOV MESSAGE
-    * Version 13.1
-    * */
+     * The following test use slightly modified messages found on page 76 and 77 of the
+     * PASSENGER AND AIRPORT DATA
+     * INTERCHANGE STANDARDS EDIFACT IMPLEMENTATION GUIDE
+     * PNR DATA PUSHED TO STATES  OR OTHER AUTHORITIES
+     * PNRGOV MESSAGE
+     * Version 13.1
+     * */
     @Test
     public void reservationDateMapsToRCITimeCreated() throws ParseException {
-       PnrVo vo = this.parser.parse(pnrTestMessages.fullPnrMessage());
-        LocalDateTime reservationDate =  Instant.ofEpochMilli(vo.getReservationCreateDate().getTime())
-                .atZone(ZoneId.systemDefault())
-                .toLocalDateTime();
-       LocalDateTime May23rd2013At181348 = LocalDateTime.of(2013, 5, 23, 18, 13, 48);
+        PnrVo vo = this.parser.parse(pnrTestMessages.fullPnrMessagePg77());
+        LocalDateTime reservationDate = getLocalDateTime(vo.getReservationCreateDate());
+        LocalDateTime May23rd2013At181348 = LocalDateTime.of(2013, 5, 23, 18, 13, 48);
         assertTrue(May23rd2013At181348.isEqual(reservationDate));
     }
 
     @Test
     public void dateBookedMapsToPaymentMadeDate() throws ParseException {
-        PnrVo vo = this.parser.parse(pnrTestMessages.fullPnrMessage());
-        LocalDateTime dateBooked =  Instant.ofEpochMilli(vo.getDateBooked().getTime())
-                .atZone(ZoneId.systemDefault())
-                .toLocalDateTime();
+        PnrVo vo = this.parser.parse(pnrTestMessages.fullPnrMessagePg77());
+        LocalDateTime dateBooked = getLocalDateTime(vo.getDateBooked());
         LocalDateTime May23rd2013At212400 = LocalDateTime.of(2013, 5, 23, 21, 24);
         assertTrue(May23rd2013At212400.isEqual(dateBooked));
+    }
+
+    @Test
+    public void bookingDateMapsToFirstDateWithCode710() throws ParseException {
+        PnrVo vo = this.parser.parse(pnrTestMessages.fullPnrMessagePg76());
+        LocalDate dateBooked = getLocalDate(vo.getDateBooked());
+        LocalDate Feb142013 = LocalDate.of(2013, 2, 14);
+        assertTrue(Feb142013.isEqual(dateBooked));
+    }
+
+    private LocalDate getLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    private LocalDateTime getLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
     }
 }
