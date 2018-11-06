927306739d8beac7f0689643e2e1abd576338ae7
Tsegay <tsegay.tekle@gmail.com>
2018-11-06 12:42:16 -0500

    update watchlist categories lookup table

--- gtas-parent/gtas-commons/src/main/resources/sql/watchlist_categories.sql
+++ gtas-parent/gtas-commons/src/main/resources/sql/watchlist_categories.sql
@@ -1,2 +1,3 @@
-insert into watch_list_category (`id`,`name`,`description`) values(1, 'Stolen Passport','Stolen Passport');
-insert into watch_list_category (`id`,`name`,`description`) values(2, 'Lost passport','Lost passport');
+insert into watch_list_category (`id`,`name`,`description`) values(1, 'General','General');
+insert into watch_list_category (`id`,`name`,`description`) values(2, 'Stolen Passport','Stolen Passport');
+insert into watch_list_category (`id`,`name`,`description`) values(3, 'Lost passport','Lost passport');
