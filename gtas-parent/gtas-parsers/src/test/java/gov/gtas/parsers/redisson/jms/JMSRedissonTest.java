593f1160bc7e1c64ce9577bcba53466ac2cb2448
Geoffrey Bays <geoffrey.bays88@gmail.com>
2019-01-25 12:06:08 -0500

    Fixed compile issue
    Fixed merge conflict and compile issue.

--- gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/redisson/jms/JMSRedissonTest.java
+++ gtas-parent/gtas-parsers/src/test/java/gov/gtas/parsers/redisson/jms/JMSRedissonTest.java
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 package gov.gtas.parsers.redisson.jms;
 
 import gov.gtas.config.CachingConfig;
 import gov.gtas.config.CommonServicesConfig;
 import gov.gtas.jms.config.JmsConfiguration;
 import gov.gtas.jms.config.MessagingListnerConfiguration;
-import gov.gtas.jms.services.MessageReceiver;
-import gov.gtas.jms.services.MessageSender;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -15,7 +12,6 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.annotation.Rollback;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
@@ -43,44 +39,4 @@ public class JMSRedissonTest {
     }
 
 }
-=======
-package gov.gtas.parsers.redisson.jms;
 
-import gov.gtas.config.CachingConfig;
-import gov.gtas.config.CommonServicesConfig;
-import gov.gtas.jms.config.JmsConfiguration;
-import gov.gtas.jms.config.MessagingListnerConfiguration;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-
-//@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { CommonServicesConfig.class,
-        CachingConfig.class,JmsConfiguration.class,MessagingListnerConfiguration.class })
-@ComponentScan("gov.gtas.parsers.redisson")
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class JMSRedissonTest {
-
-    @Autowired
-    private InboundQMessageSender sender;
-
-    @Autowired
-    private InboundQMessageListener receiver;
-
-    //@Test
-    public void testJMSPostRedis(){
-        Assert.assertTrue(true);
-    }
-
-    //@Test
-    public void testJMSPreRedis(){
-        Assert.assertTrue(true);
-    }
-
-}
->>>>>>> dev
