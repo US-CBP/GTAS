5e226978bfdfa4225d4546e9fdbf5506ce5c04dd
Robert Stine <RobWStine@outlook.com>
2019-03-05 10:20:04 -0500

    Code cleanup - remove flight repository warnings.

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightRepository.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightRepository.java
@@ -12,14 +12,11 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import gov.gtas.model.CodeShareFlight;
 import gov.gtas.model.Flight;
-import gov.gtas.model.Passenger;
 
 public interface FlightRepository extends JpaRepository<Flight, Long>, FlightRepositoryCustom {
     @Query("SELECT f FROM Flight f WHERE f.carrier = :carrier "
@@ -27,7 +24,7 @@ public interface FlightRepository extends JpaRepository<Flight, Long>, FlightRep
             + "AND f.origin = :origin "
             + "AND f.destination=:destination "
             + "AND f.flightDate = :flightDate")
-    public Flight getFlightByCriteria(@Param("carrier") String carrier,
+    Flight getFlightByCriteria(@Param("carrier") String carrier,
             @Param("flightNumber") String flightNumber,@Param("origin") String origin,
             @Param("destination") String destination,@Param("flightDate") Date flightDate);
 
@@ -37,30 +34,32 @@ public interface FlightRepository extends JpaRepository<Flight, Long>, FlightRep
      * sufficient.  Use this method as a last resort to compare 
      * flight dates. 
      */
+/*
     @Query("SELECT f FROM Flight f WHERE f.carrier = :carrier "
             + "AND f.flightNumber = :flightNumber "
             + "AND f.origin = :origin "
             + "AND f.destination=:destination "
             + "AND f.flightDate between :startDate AND :endDate")
-    public Flight getFlightByCriteria(
+    Flight getFlightByCriteria(
             @Param("carrier") String carrier,
             @Param("flightNumber") String flightNumber,
             @Param("origin") String origin,
             @Param("destination") String destination,
             @Param("startDate") Date startDate,
             @Param("endDate") Date endDate);
+*/
 
-    public Page<Flight> findAll(Pageable pageable);
+    Page<Flight> findAll(Pageable pageable);
     
     /*@Query("SELECT f FROM Flight f join f.passengers p where p.id = (:paxId)")
     public List<Flight> getFlightByPaxId(@Param("paxId") Long paxId);*/
     
     @Query(nativeQuery = true, 
-    		value="SELECT f.* FROM flight_passenger fp join flight f ON (fp.flight_id = f.id) where fp.pax_id = (:paxId)")
-    public List<Flight> getFlightByPaxId(@Param("paxId") Long paxId);
+    		value="SELECT f.* FROM flight_passenger fp join flight f ON (fp.flight_id = f.id) where fp.passenger_id = (:paxId)")
+    List<Flight> getFlightByPaxId(@Param("paxId") Long paxId);
 
     @Query("SELECT f FROM Flight f WHERE f.flightDate between :startDate AND :endDate")
-    public List<Flight> getFlightsByDates(@Param("startDate") Date startDate,
+    List<Flight> getFlightsByDates(@Param("startDate") Date startDate,
                                           @Param("endDate") Date endDate);
 
     /*@Query("SELECT f FROM Flight f join f.passengers p join p.documents d where UPPER(p.firstName) = UPPER(:firstName) "
@@ -72,15 +71,15 @@ public interface FlightRepository extends JpaRepository<Flight, Long>, FlightRep
                                                              @Param("documentNumber") String documentNumber);*/
     
     @Query(nativeQuery = true, 
-    		value="SELECT f.* FROM flight_passenger fp JOIN Flight f ON (fp.flight_id = f.id) JOIN Passenger p ON(fp.passenger_Id = p.id )"
-    				+ " JOIN Document d ON (p.id = d.passenger_id) where d.document_number=(:documentNumber) AND UPPER(p.first_name) = UPPER(:firstName)"
+    		value="SELECT f.* FROM flight_passenger fp JOIN flight f ON (fp.flight_id = f.id) JOIN passenger p ON(fp.passenger_Id = p.id )"
+    				+ " JOIN document d ON (p.id = d.passenger_id) where d.document_number=(:documentNumber) AND UPPER(p.first_name) = UPPER(:firstName)"
     				+ " AND UPPER(p.last_name) = UPPER(:lastName)")
-    public List<Flight> getFlightsByPassengerNameAndDocument(@Param("firstName") String firstName,
+    List<Flight> getFlightsByPassengerNameAndDocument(@Param("firstName") String firstName,
 													         @Param("lastName") String lastName,
 													         @Param("documentNumber") String documentNumber);
 
     @Query("SELECT c FROM CodeShareFlight c where c.operatingFlightId = :flightId group by c.marketingFlightNumber")
-    public List<CodeShareFlight> getCodeSharesForFlight(@Param("flightId") Long flightId);
+    List<CodeShareFlight> getCodeSharesForFlight(@Param("flightId") Long flightId);
     
     @Query( "SELECT DISTINCT f FROM Flight f " +
             "LEFT JOIN FETCH f.passengers pass " +
@@ -91,6 +90,6 @@ public interface FlightRepository extends JpaRepository<Flight, Long>, FlightRep
             "AND (((SELECT MIN(pwts.watchlistCheckTimestamp) from pwts where pass.flight.id = f.id and pwts.id = pass.id ) <= (SELECT MAX(wl.editTimestamp) FROM Watchlist wl)) " +
             "       OR (((SELECT COUNT(pass.id) FROM pass WHERE pass.flight.id = f.id AND pass.id NOT IN (SELECT id FROM pwts)) > 0) " +
                                 " OR (SELECT COUNT(pass.id) FROM pass WHERE pass.flight.id = f.id AND pass.id = pwts.id AND pwts.watchlistCheckTimestamp IS NULL) > 0))")
-    public List<Flight> getInboundAndOutboundFlightsWithinTimeFrame(@Param("dateTimeStart")Date date1,
+    List<Flight> getInboundAndOutboundFlightsWithinTimeFrame(@Param("dateTimeStart")Date date1,
     											  @Param("dateTimeEnd") Date date2);
 }
