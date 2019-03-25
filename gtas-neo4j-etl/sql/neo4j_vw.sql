decfc44b0a1a1f6764cc9493aa699ed1aa7d2834
Teddy <Teddy@192.168.86.54>
2019-03-25 17:31:12 -0400

    Updated scripts which creates the neo4j_vw

--- gtas-neo4j-etl/sql/neo4j_vw.sql
+++ gtas-neo4j-etl/sql/neo4j_vw.sql
@@ -64,10 +64,10 @@ SELECT pit.idTag,
 	
 FROM gtas.passenger_id_tag pit
 INNER JOIN gtas.passenger p ON pit.pax_id = p.id 
-INNER JOIN gtas.passenger_details pd ON p.id = pd.pd_passenger_id
-INNER JOIN gtas.passenger_trip_details ptd ON p.id = ptd.ptd_id
 INNER JOIN gtas.flight_passenger fp ON fp.passenger_id = p.id
 INNER JOIN gtas.flight f ON f.id = fp.flight_id
+INNER JOIN gtas.passenger_details pd ON p.id = pd.pd_passenger_id
+INNER JOIN gtas.passenger_trip_details ptd ON p.id = ptd.ptd_id
 LEFT JOIN  gtas.flight_passenger_count fpc ON fpc.fp_flight_id = fp.flight_id
 LEFT JOIN gtas.flight_hit_watchlist fhw ON fhw.fhw_flight_id = f.id
 LEFT JOIN gtas.flight_hit_rule fhr ON fhr.fhr_flight_id = f.id
