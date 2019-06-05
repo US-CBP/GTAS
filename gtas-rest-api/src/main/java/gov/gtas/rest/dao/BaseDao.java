072544576d1cfe8aa89ccb0b82acd1c067e80e62
Robert Stine <RobWStine@outlook.com>
2019-06-05 13:50:11 -0400

    Removing the GTAS API. It is not in use and has several security violations.

--- gtas-rest-api/src/main/java/gov/gtas/rest/dao/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package gov.gtas.rest.dao;
-
-import gov.gtas.rest.request.BaseRequest;
-import gov.gtas.rest.response.BaseResponse;
-
-public interface BaseDao <R extends BaseRequest<?>, P extends BaseResponse<?> >{
-
-	P findByID (R r);
-}
