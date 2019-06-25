23c07fbc6827417eab78e33ce9ddb702e67440c4
Tsegay <tsegay.tekle@gmail.com>
2019-06-25 14:01:29 -0400

    add flight itinerary visualization, update kibana import/export scripts to inlude the new visualization

--- gtas-parent/scripts/elastic/config/kibana/kibana.export-dashboard.sh
+++ gtas-parent/scripts/elastic/config/kibana/kibana.export-dashboard.sh
@@ -12,5 +12,10 @@ KIBANA_HOST='localhost'
 #Default port
 KIBANA_PORT=5601
 
-# 
+# export default gtas dashbaord
 curl -X GET "http://$KIBANA_HOST:$KIBANA_PORT/api/kibana/dashboards/export?dashboard=7cfbbdc0-2e13-11e9-81a3-0f5bd8b0a7ac" -H 'kbn-xsrf: true' -o $CURRENT_DIR/kibana.default-dashboard.json
+
+# export flight itinerary visualization
+
+curl -X GET "http://$KIBANA_HOST:$KIBANA_PORT/api/saved_objects/visualization/29ac1380-66a9-11e9-9ffd-9d63a89be4bb" -H 'kbn-xsrf: true' -o $CURRENT_DIR/kibana.flight-itinerary-visualization.json 
+
--- gtas-parent/scripts/elastic/config/kibana/kibana.import-dashboard.sh
+++ gtas-parent/scripts/elastic/config/kibana/kibana.import-dashboard.sh
@@ -23,11 +23,13 @@ DEFAULT_INDEX_ID="96df0890-2ba8-11e9-a5e4-2bbcf61c6cb1"
 # (./kibana.export-dashboard.sh will export and overrite kibana.default-dashboard.json). 
 # The dashboard exported using the web UI will not work here since it has slightly different format
 
-echo 'importing dashboard ....'
+echo 'importing  ....'
 
 curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "$URL/api/kibana/dashboards/import?force=true" -d @kibana.default-dashboard.json
 
-echo 'dashboard imported!!'
+curl -X POST -H "Content-Type: application/json" -H "kbn-xsrf: true" "$URL/api/saved_objects/visualization/29ac1380-66a9-11e9-9ffd-9d63a89be4bb?force=true" -d $CURRENT_DIR/kibana.flight-itinerary-visualization.json
+
+echo ' imported!!'
 
 echo "making $DEFAULT_INDEX_ID the default index pattern"
 # sets flightpax as the default index pattern 
