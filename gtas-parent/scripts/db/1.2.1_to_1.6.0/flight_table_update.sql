60c5cbd137195ff7ad46135912bfbdab55777771
Robert Stine <RobWStine@outlook.com>
2019-08-26 10:55:52 -0400

    #1196 Update flight table to remove the unique key.

--- gtas-parent/scripts/db/1.2.1_to_1.6.0/flight_table_update.sql
+++ gtas-parent/scripts/db/1.2.1_to_1.6.0/flight_table_update.sql
@@ -1,5 +1,4 @@
 
 -- Add id_tag to the FLIGHT table
 ALTER TABLE `flight`
-	ADD COLUMN IF NOT EXISTS `id_tag` VARCHAR(255) NULL DEFAULT NULL AFTER `full_flight_number`,
-	ADD UNIQUE INDEX IF NOT EXISTS `UK_FlightIdTag` (`id_tag`);
+	ADD COLUMN IF NOT EXISTS `id_tag` VARCHAR(255) NULL DEFAULT NULL AFTER `full_flight_number`;
