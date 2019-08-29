343788144da56c3520b87ab8e7b8b85b2d9c3b4e
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2019-08-29 13:34:30 -0400

    initial to add trip type 'non-contiguous', some refactoring

---
+++ gtas-parent/gtas-commons/src/test/java/gov/gtas/enums/enumTests.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *  * All Application code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+ *  *
+ *  * Please see LICENSE.txt for details.
+ *
+ */
+
+
+package gov.gtas.enums;
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+
+import gov.gtas.enumtype.TripTypeEnum;
+
+public class enumTests {
+
+    @Test
+    public void tripTypeTest() {
+        TripTypeEnum ttm = TripTypeEnum.MULTICITY;
+        String mc = ttm.toString();
+        Assert.isTrue(mc.equals("MULTI-CITY"), "Multi-city trip type text does not equal `MULTI-CITY`");
+    
+        TripTypeEnum ttr = TripTypeEnum.ROUNDTRIP;
+        String rt = ttr.toString();
+        Assert.isTrue(rt.equals("ROUND-TRIP"), "ROUNDTRIP trip type text does not equal `ROUND-TRIP`");
+
+        TripTypeEnum tto = TripTypeEnum.ONEWAY;
+        String ow = tto.toString();
+        Assert.isTrue(ow.equals("ONE-WAY"), "ONEWAY trip type text does not equal `ONE-WAY`");
+
+        TripTypeEnum ttj = TripTypeEnum.OPENJAW;
+        String oj = ttj.toString();
+        Assert.isTrue(oj.equals("OPEN JAW"), "OPENJAW trip type text does not equal `OPEN JAW`");
+
+        TripTypeEnum ttn = TripTypeEnum.NONCONTIGUOUS;
+        String nc = ttn.toString();
+        Assert.isTrue(nc.equals("NON-CONTIGUOUS"), "NONCONTIGUOUS trip type text does not equal `NON-CONTIGUOUS`");
+      }
+}
