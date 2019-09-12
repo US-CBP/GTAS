3fd7e11a018b8cb6124a308304ea67c846862576
Kasim Helil <kasimhelil@gmail.com>
2019-09-12 17:43:49 -0400

    Updated passenger entity resolution

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightPassengerRepository.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightPassengerRepository.java
@@ -1,6 +1,7 @@
 package gov.gtas.repository;
 
 import gov.gtas.model.FlightPassenger;
+import java.util.Date;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -38,9 +39,67 @@ public interface FlightPassengerRepository extends CrudRepository<FlightPassenge
              "LEFT JOIN FETCH pax.passengerTripDetails " +
              "WHERE fp.flightId = :flightId " +
              "AND :recordLocator IN (SELECT pnr.recordLocator FROM fp.flight.pnrs pnr) " +
-             "AND fp.passenger.passengerTripDetails.pnrReservationReferenceNumber = :pnrReservationReferenceNumber ")
-     List<FlightPassenger> getPassengerUsingREF(@NonNull @Param("flightId") Long flightId, 
+             "AND pax.passengerTripDetails.pnrReservationReferenceNumber = :pnrReservationReferenceNumber ")
+     List<FlightPassenger> getPassengerUsingREF(
+    		 @NonNull @Param("flightId") Long flightId, 
     		 @NonNull @Param("pnrReservationReferenceNumber")String pnrReservationReferenceNumber,
     		 @NonNull @Param("recordLocator")String recordLocator);
- 
+     
+     
+     @Query("SELECT fp FROM FlightPassenger fp " +
+             "JOIN FETCH fp.passenger pax " +
+             "LEFT JOIN FETCH pax.documents " +
+             "LEFT JOIN FETCH pax.seatAssignments " +
+             "LEFT JOIN FETCH pax.bags " +
+             "LEFT JOIN FETCH pax.flightPaxList " +
+             "LEFT JOIN FETCH pax.tickets " +
+             "LEFT JOIN FETCH pax.bookingDetails " +
+             "LEFT JOIN FETCH pax.passengerDetails " +
+             "LEFT JOIN FETCH pax.passengerTripDetails " +
+             "WHERE pax.passengerIDTag.idTag = :passengerIDTag " +
+             "AND fp.flightId = :flightId")
+     List<FlightPassenger> getPassengerByIdTag(
+    		 @NonNull @Param("flightId") Long flightId,
+    		 @NonNull @Param("passengerIDTag") String passengerIDTag);
+     
+     @Query("SELECT fp FROM FlightPassenger fp " +
+             "JOIN FETCH fp.passenger pax " +
+             "LEFT JOIN FETCH pax.documents " +
+             "LEFT JOIN FETCH pax.seatAssignments " +
+             "LEFT JOIN FETCH pax.bags " +
+             "LEFT JOIN FETCH pax.flightPaxList " +
+             "LEFT JOIN FETCH pax.tickets " +
+             "LEFT JOIN FETCH pax.bookingDetails " +
+             "LEFT JOIN FETCH pax.passengerDetails " +
+             "LEFT JOIN FETCH pax.passengerTripDetails " +
+             "WHERE (UPPER(fp.passenger.passengerDetails.firstName) = UPPER(:firstName) " +
+             "AND UPPER(fp.passenger.passengerDetails.lastName) = UPPER(:lastName) " +
+             "AND fp.passenger.passengerDetails.dob = :dob) " +
+             "AND fp.flightId = :flightId")
+     List<FlightPassenger> getPassengerByFirstNameLastNameAndDOB(
+    		 @NonNull @Param("flightId") Long flightId,
+    		 @NonNull @Param("firstName") String firstName, 
+    		 @NonNull @Param("lastName") String lastName,
+    		 @NonNull @Param("dob") Date dob);
+     
+     
+     @Query("SELECT fp FROM FlightPassenger fp " +
+             "JOIN FETCH fp.passenger pax " +
+             "LEFT JOIN FETCH pax.documents " +
+             "LEFT JOIN FETCH pax.seatAssignments " +
+             "LEFT JOIN FETCH pax.bags " +
+             "LEFT JOIN FETCH pax.flightPaxList " +
+             "LEFT JOIN FETCH pax.tickets " +
+             "LEFT JOIN FETCH pax.bookingDetails " +
+             "LEFT JOIN FETCH pax.passengerDetails " +
+             "LEFT JOIN FETCH pax.passengerTripDetails " +
+             "WHERE (UPPER(fp.passenger.passengerDetails.firstName) = UPPER(:firstName) " +
+             "AND UPPER(fp.passenger.passengerDetails.lastName) = UPPER(:lastName) " +
+             "AND UPPER(fp.passenger.passengerDetails.gender) = UPPER(:gender)) " +
+             "AND fp.flightId = :flightId")
+     List<FlightPassenger> getPassengerByFirstNameLastNameAndGender(
+    		 @NonNull @Param("flightId") Long flightId,
+    		 @NonNull @Param("firstName") String firstName, 
+    		 @NonNull @Param("lastName") String lastName,
+    		 @NonNull @Param("gender") String gender);
 }
--- gtas-parent/gtas-loader/src/main/java/gov/gtas/services/ServiceUtil.java
+++ gtas-parent/gtas-loader/src/main/java/gov/gtas/services/ServiceUtil.java
@@ -13,66 +13,138 @@ import gov.gtas.model.lookup.Airport;
 import gov.gtas.parsers.vo.PassengerVo;
 import gov.gtas.repository.FlightPassengerRepository;
 import gov.gtas.repository.PassengerRepository;
+import gov.gtas.util.EntityResolverUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class ServiceUtil implements LoaderServices {
+	private static final Logger logger = LoggerFactory.getLogger(ServiceUtil.class);
 
-    @Autowired
-    private AirportService airportService;
+	@Value("${allow.loosen.resolution}")
+	private Boolean allowLoosenResolution;
 
-    @Autowired
-    PassengerRepository passengerRepository;
+	@Autowired
+	private AirportService airportService;
 
-    @Autowired
-    FlightPassengerRepository flightPassengerRepository;
+	@Autowired
+	PassengerRepository passengerRepository;
 
-    public String getCountry(String airport) {
+	@Autowired
+	FlightPassengerRepository flightPassengerRepository;
 
-        Airport a = airportService.getAirportByThreeLetterCode(airport);
-        if (a == null) {
-            return "USA";
-        }
-        return a.getCountry();
+	public String getCountry(String airport) {
 
-    }
+		Airport a = airportService.getAirportByThreeLetterCode(airport);
+		if (a == null) {
+			return "USA";
+		}
+		return a.getCountry();
 
-    public AirportService getAirportService() {
-        return airportService;
-    }
+	}
 
-    public void setAirportService(AirportService airportService) {
-        this.airportService = airportService;
-    }
+	public AirportService getAirportService() {
+		return airportService;
+	}
 
-    @Override
-    @Transactional()
-    public Passenger findPassengerOnFlight(Flight f, PassengerVo pvo) {
-        if (f.getId() == null) {
-            return null;
-        }
-        List<FlightPassenger> pax = null;
-        
-        //Query passenger by Flight -> pnr recordLocator# -> pnr REF number       
-        if (pvo.getPnrRecordLocator() != null && pvo.getPnrReservationReferenceNumber() != null) {
-        	 pax = flightPassengerRepository.getPassengerUsingREF(f.getId(),
-                    pvo.getPnrReservationReferenceNumber(), pvo.getPnrRecordLocator());
-        }
-        else {
-        	pax = flightPassengerRepository.returnAPassengerFromParameters(f.getId(),
-                    pvo.getFirstName(), pvo.getLastName());
-        }
+	public void setAirportService(AirportService airportService) {
+		this.airportService = airportService;
+	}
 
-        
-    if (pax != null && pax.size() >= 1) {
-            return pax.get(0).getPassenger();
-        } else {
-            return null;
-        }
-    }
+
+	private String createPassengerIdTag(PassengerVo pvo) {
+		String input = String.join("",Arrays.asList(
+				pvo.getFirstName().toUpperCase(), 
+				pvo.getLastName().toUpperCase(), 
+				pvo.getGender().toUpperCase(), 
+				new SimpleDateFormat("MM/dd/yyyy").format(pvo.getDob())));
+
+		String passengerIdTag = null;
+
+		try {
+			passengerIdTag = EntityResolverUtils.makeSHA1Hash(input);			
+		} catch (NoSuchAlgorithmException e) {
+			logger.error("ERROR! An error occured when trying to create passengerIdTag", e);
+		} catch (UnsupportedEncodingException e) {
+			logger.error("ERROR! An error occured when trying to create passengerIdTag", e);
+		}
+
+		return passengerIdTag;
+
+	}
+
+	@Override
+	@Transactional()
+	public Passenger findPassengerOnFlight(Flight f, PassengerVo pvo) {
+		if (f.getId() == null) {
+			return null;
+		}
+		List<FlightPassenger> pax = null;
+		Long flightId = f.getId();
+		String firstName = pvo.getFirstName();
+		String lastName = pvo.getLastName();
+		Date dob = pvo.getDob();
+		String gender = pvo.getGender();
+		String recordLocator = pvo.getPnrRecordLocator();
+		String ref = pvo.getPnrReservationReferenceNumber();
+		boolean found = false;
+
+		//Resolve passenger by Flight -> pnr recordLocator# -> pnr REF number       
+		if (recordLocator != null && ref != null) {
+			pax = flightPassengerRepository.getPassengerUsingREF(flightId, ref, recordLocator);	
+			found = (pax != null && pax.size() > 0);
+		}
+
+
+		//Resolve passenger by passengerIdTAg
+		if (!found && pvo.getGender() != null && pvo.getDob() != null) {			
+			String passengerIdTag = createPassengerIdTag(pvo);
+			pax = flightPassengerRepository.getPassengerByIdTag(f.getId(), passengerIdTag);
+			found = (pax != null && pax.size() > 0);
+
+		}
+		if (!found && allowLoosenResolution) {
+			//Resolve passenger by first name, last name and dob
+			if (pvo.getDob() != null) {
+				pax = flightPassengerRepository
+						.getPassengerByFirstNameLastNameAndDOB(flightId, firstName, lastName, dob);
+				found = (pax != null && pax.size() > 0);
+			}
+
+			//Resolve passenger by first name, last name, and gender
+			if (!found && pvo.getGender() != null) {
+				pax = flightPassengerRepository
+						.getPassengerByFirstNameLastNameAndGender(flightId, firstName, lastName, gender);
+				found = (pax != null && pax.size() > 0);
+			}
+
+			//Resolve by first name and last name
+			if(!found){
+				pax = flightPassengerRepository
+						.returnAPassengerFromParameters(flightId, firstName, lastName);
+				found = (pax != null && pax.size() > 0);
+			}
+		}
+
+		if (found) {
+			return pax.get(0).getPassenger();
+		} else {
+			return null;
+		}
+	}
 }
 
