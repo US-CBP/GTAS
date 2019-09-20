70fdf6bc9944f93f16032e8267bfa7441d418357
Robert Stine <RobWStine@outlook.com>
2019-09-20 12:08:12 -0400

    #1403 further updates. Use passenger repository instead of flight passenger repo to get a passenger.

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightPassengerRepository.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/FlightPassengerRepository.java
@@ -1,47 +1,7 @@
 package gov.gtas.repository;
 
 import gov.gtas.model.FlightPassenger;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
-
-import java.util.List;
 
 public interface FlightPassengerRepository extends CrudRepository<FlightPassenger, Long> {
-
-     @Query("SELECT fp FROM FlightPassenger fp " +
-            "JOIN FETCH fp.passenger pax " +
-            "LEFT JOIN FETCH pax.documents " +
-            "LEFT JOIN FETCH pax.seatAssignments " +
-            "LEFT JOIN FETCH pax.bags " +
-            "LEFT JOIN FETCH pax.flightPaxList " +
-            "LEFT JOIN FETCH pax.tickets " +
-            "LEFT JOIN FETCH pax.bookingDetails " +
-            "LEFT JOIN FETCH pax.passengerDetails " +
-            "LEFT JOIN FETCH pax.passengerTripDetails " +
-            "WHERE (UPPER(fp.passenger.passengerDetails.firstName) = UPPER(:firstName) " +
-            "AND UPPER(fp.passenger.passengerDetails.lastName) = UPPER(:lastName)) " +
-            "AND fp.flightId = :flightId")
-    List<FlightPassenger> returnAPassengerFromParameters(@Param("flightId") Long flightId, @Param("firstName")String firstName, @Param("lastName")String lastName);
-    
-     
-     @Query("SELECT fp FROM FlightPassenger fp " +
-             "JOIN FETCH fp.passenger pax " +
-             "LEFT JOIN FETCH pax.documents " +
-             "LEFT JOIN FETCH pax.seatAssignments " +
-             "LEFT JOIN FETCH pax.bags " +
-             "LEFT JOIN FETCH pax.flightPaxList " +
-             "LEFT JOIN FETCH pax.tickets " +
-             "LEFT JOIN FETCH pax.bookingDetails " +
-             "LEFT JOIN FETCH pax.passengerDetails " +
-             "LEFT JOIN FETCH pax.passengerTripDetails " +
-             "WHERE fp.flightId = :flightId " +
-             "AND :recordLocator IN (SELECT pnr.recordLocator FROM fp.flight.pnrs pnr) " +
-             "AND pax.passengerTripDetails.pnrReservationReferenceNumber = :pnrReservationReferenceNumber ")
-     List<FlightPassenger> getPassengerUsingREF(
-    		 @NonNull @Param("flightId") Long flightId, 
-    		 @NonNull @Param("pnrReservationReferenceNumber")String pnrReservationReferenceNumber,
-    		 @NonNull @Param("recordLocator")String recordLocator);
-     
 }
