eb6613c11c64fd1628ecc1f087c98b40e6b683ef
Robert Stine <RobWStine@outlook.com>
2019-09-25 09:48:36 -0400

    Updating formatting to use eclipse.

--- gtas-parent/gtas-job-scheduler-war/src/main/java/gov/gtas/job/scheduler/RuleRunnerThread.java
+++ gtas-parent/gtas-job-scheduler-war/src/main/java/gov/gtas/job/scheduler/RuleRunnerThread.java
@@ -38,99 +38,105 @@ import static gov.gtas.repository.AppConfigurationRepository.FUZZY_MATCHING;
 @Scope("prototype")
 public class RuleRunnerThread implements Callable<Boolean> {
 
+	private Logger logger = LoggerFactory.getLogger(RuleRunnerThread.class);
 
-    private Logger logger = LoggerFactory.getLogger(RuleRunnerThread.class);
+	private final ApplicationContext applicationContext;
+	/**
+	 * The error persistence service.
+	 */
+	private ErrorPersistenceService errorPersistenceService;
 
-    private final ApplicationContext applicationContext;
-    /**
-     * The error persistence service.
-     */
-    private ErrorPersistenceService errorPersistenceService;
+	private final AppConfigurationService appConfigurationService;
 
-    private final AppConfigurationService appConfigurationService;
+	private List<MessageStatus> messageStatuses = new ArrayList<>();
 
-    private List<MessageStatus> messageStatuses = new ArrayList<>();
+	public RuleRunnerThread(ErrorPersistenceService errorPersistenceService,
+			AppConfigurationService appConfigurationService, ApplicationContext applicationContext) {
+		this.errorPersistenceService = errorPersistenceService;
+		this.appConfigurationService = appConfigurationService;
+		this.applicationContext = applicationContext;
+	}
 
+	public Boolean call() {
+		logger.debug("Starting rule running scheduled task");
+		boolean success = true;
+		long start = System.nanoTime();
+		RuleResultsWithMessageStatus ruleResults = null;
+		TargetingService targetingService = applicationContext.getBean(TargetingService.class);
+		try {
+			ruleResults = targetingService.analyzeLoadedMessages(messageStatuses);
+			List<TargetingServiceResults> targetingServiceResultsList = targetingService
+					.createHitsAndCases(ruleResults.getRuleResults());
+			logger.debug("About to batch");
+			int BATCH_SIZE = Integer.parseInt(appConfigurationService
+					.findByOption(AppConfigurationRepository.MAX_FLIGHTS_SAVED_PER_BATCH).getValue());
+			List<TargetingServiceResults> batchedTargetingServiceResults = TargetingResultUtils
+					.batchResults(targetingServiceResultsList, BATCH_SIZE);
+			logger.debug("done batching");
+			int count = 1;
+			if (ruleResults.getMessageStatusList() != null) {
+				Date analyzedAt = new Date();
+				ruleResults.getMessageStatusList().forEach(m -> {
+					m.setMessageStatusEnum(MessageStatusEnum.ANALYZED);
+					m.setAnalyzedTimestamp(analyzedAt);
+				});
+				for (TargetingServiceResults targetingServiceResults : batchedTargetingServiceResults) {
+					try {
+						logger.info("Saving rules/summary targeting results " + count + " of "
+								+ batchedTargetingServiceResults.size() + "...");
+						targetingService.saveEverything(targetingServiceResults);
+					} catch (Exception ignored) {
+						ruleResults.getMessageStatusList()
+								.forEach(m -> m.setMessageStatusEnum(MessageStatusEnum.PARTIAL_ANALYZE));
+						logger.error("Failed to save rules summary count " + count + " with following stacktrace: ",
+								ignored);
+					}
+					count++;
+				}
+				logger.info("Rules and Watchlist ran in " + (System.nanoTime() - start) / 1000000 + "m/s.");
+			}
+			logger.debug("entering matching service portion of jobScheduling");
+			boolean fuzzyMatchingOn = false;
+			try {
+				fuzzyMatchingOn = Boolean.parseBoolean(appConfigurationService.findByOption(FUZZY_MATCHING).getValue());
+			} catch (Exception e) {
+				logger.warn("FUZZY MATCHING NOT CONFIGURED - DEFAULT WILL NOT RUN - FUZZY MATCHING IS OFF!"
+						+ "\n SET FUZZY MATCHING IN APPLICATION CONFIG");
+			}
+			if (fuzzyMatchingOn) {
+				long fuzzyStart = System.nanoTime();
+				MatchingService matchingService = applicationContext.getBean(MatchingService.class);
+				int fuzzyHits = matchingService.findMatchesBasedOnTimeThreshold(messageStatuses);
+				logger.debug("exiting matching service portion of jobScheduling");
+				if (fuzzyHits > 0) {
+					logger.info("Fuzzy Matching had " + fuzzyHits + " hits and Ran in  "
+							+ (System.nanoTime() - fuzzyStart) / 1000000 + "m/s.");
+				}
+			}
+			if (ruleResults.getMessageStatusList() != null) {
+				targetingService.saveMessageStatuses(ruleResults.getMessageStatusList());
+			}
+			logger.debug("Total rule running scheduled task took  " + (System.nanoTime() - start) / 1000000 + "m/s.");
+		} catch (Exception exception) {
+			String errorMessage = exception.getCause() != null && exception.getCause().getMessage() != null
+					? exception.getCause().getMessage()
+					: "Error in rule runner";
+			logger.error(errorMessage);
+			if (ruleResults != null) {
+				ruleResults.getMessageStatusList()
+						.forEach(m -> m.setMessageStatusEnum(MessageStatusEnum.FAILED_ANALYZING));
+				targetingService.saveMessageStatuses(ruleResults.getMessageStatusList());
+			}
+			ErrorDetailInfo errInfo = ErrorHandlerFactory
+					.createErrorDetails(RuleServiceConstants.RULE_ENGINE_RUNNER_ERROR_CODE, exception);
+			errorPersistenceService.create(errInfo);
+			success = false;
+		}
+		logger.debug("exiting jobScheduling()");
+		return success;
+	}
 
-    public RuleRunnerThread(ErrorPersistenceService errorPersistenceService,
-                            AppConfigurationService appConfigurationService, ApplicationContext applicationContext) {
-        this.errorPersistenceService = errorPersistenceService;
-        this.appConfigurationService = appConfigurationService;
-        this.applicationContext = applicationContext;
-    }
-
-
-    public Boolean call() {
-        logger.debug("Starting rule running scheduled task");
-        boolean success = true;
-        long start = System.nanoTime();
-        RuleResultsWithMessageStatus ruleResults = null;
-        TargetingService targetingService = applicationContext.getBean(TargetingService.class);
-        try {
-            ruleResults = targetingService.analyzeLoadedMessages(messageStatuses);
-            List<TargetingServiceResults> targetingServiceResultsList = targetingService.createHitsAndCases(ruleResults.getRuleResults());
-            logger.debug("About to batch");
-            int BATCH_SIZE = Integer.parseInt(appConfigurationService.findByOption(AppConfigurationRepository.MAX_FLIGHTS_SAVED_PER_BATCH).getValue());
-            List<TargetingServiceResults> batchedTargetingServiceResults = TargetingResultUtils.batchResults(targetingServiceResultsList, BATCH_SIZE);
-            logger.debug("done batching");
-            int count = 1;
-            if (ruleResults.getMessageStatusList() != null) {
-                Date analyzedAt = new Date();
-                ruleResults.getMessageStatusList().forEach(m -> {
-                    m.setMessageStatusEnum(MessageStatusEnum.ANALYZED);
-                    m.setAnalyzedTimestamp(analyzedAt);
-                });
-                for (TargetingServiceResults targetingServiceResults : batchedTargetingServiceResults) {
-                    try {
-                        logger.info("Saving rules/summary targeting results " + count + " of " + batchedTargetingServiceResults.size() + "...");
-                        targetingService.saveEverything(targetingServiceResults);
-                    } catch (Exception ignored) {
-                        ruleResults.getMessageStatusList().forEach(m -> m.setMessageStatusEnum(MessageStatusEnum.PARTIAL_ANALYZE));
-                        logger.error("Failed to save rules summary count " + count + " with following stacktrace: ", ignored);
-                    }
-                    count++;
-                }
-                logger.info("Rules and Watchlist ran in " + (System.nanoTime() - start) / 1000000 + "m/s.");
-            }
-            logger.debug("entering matching service portion of jobScheduling");
-            boolean fuzzyMatchingOn = false;
-            try {
-                fuzzyMatchingOn = Boolean.parseBoolean(appConfigurationService.findByOption(FUZZY_MATCHING).getValue());
-            } catch (Exception e) {
-                logger.warn("FUZZY MATCHING NOT CONFIGURED - DEFAULT WILL NOT RUN - FUZZY MATCHING IS OFF!" +
-                        "\n SET FUZZY MATCHING IN APPLICATION CONFIG");
-            }
-            if (fuzzyMatchingOn) {
-                long fuzzyStart = System.nanoTime();
-                MatchingService matchingService = applicationContext.getBean(MatchingService.class);
-                int fuzzyHits = matchingService.findMatchesBasedOnTimeThreshold(messageStatuses);
-                logger.debug("exiting matching service portion of jobScheduling");
-                if (fuzzyHits > 0) {
-                    logger.info("Fuzzy Matching had " + fuzzyHits + " hits and Ran in  " + (System.nanoTime() - fuzzyStart) / 1000000 + "m/s.");
-                }
-            }
-            if (ruleResults.getMessageStatusList() != null) {
-                targetingService.saveMessageStatuses(ruleResults.getMessageStatusList());
-            }
-            logger.debug("Total rule running scheduled task took  " + (System.nanoTime() - start) / 1000000 + "m/s.");
-        } catch (Exception exception) {
-            String errorMessage = exception.getCause() != null && exception.getCause().getMessage() != null ? exception.getCause().getMessage() : "Error in rule runner";
-            logger.error(errorMessage);
-            if (ruleResults != null) {
-                ruleResults.getMessageStatusList().forEach(m -> m.setMessageStatusEnum(MessageStatusEnum.FAILED_ANALYZING));
-                targetingService.saveMessageStatuses(ruleResults.getMessageStatusList());
-            }
-            ErrorDetailInfo errInfo = ErrorHandlerFactory
-                    .createErrorDetails(RuleServiceConstants.RULE_ENGINE_RUNNER_ERROR_CODE, exception);
-            errorPersistenceService.create(errInfo);
-            success = false;
-        }
-        logger.debug("exiting jobScheduling()");
-        return success;
-    }
-
-
-    void setMessageStatuses(List<MessageStatus> messageStatuses) {
-        this.messageStatuses = messageStatuses;
-    }
+	void setMessageStatuses(List<MessageStatus> messageStatuses) {
+		this.messageStatuses = messageStatuses;
+	}
 }
