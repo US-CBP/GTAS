0ba258bb1320654491b10d30e29a0c7cd9832532
Robert Stine <RobWStine@outlook.com>
2019-09-25 12:27:44 -0400

    GTAS Config - Update pdi-parser to use eclipse defaults

--- gtas-parent/gtas-pdi-parser/src/main/java/gov/gtas/parser/pentaho/util/ApisFlightUtil.java
+++ gtas-parent/gtas-pdi-parser/src/main/java/gov/gtas/parser/pentaho/util/ApisFlightUtil.java
@@ -1,115 +1,99 @@
 package gov.gtas.parser.pentaho.util;
 
-
-
-
-
 /**
- * This class implements methods to compute or extract flight related information
- * from an input a string. 
+ * This class implements methods to compute or extract flight related
+ * information from an input a string.
  *
  */
 public class ApisFlightUtil {
-	
-	
+
 	/**
 	 * @param firstAirportInDestCtry
-	 *           	The first airport in destination Country
+	 *            The first airport in destination Country
 	 * @param nextAirportInDestCtry
-	 *            	The next airport in destination country
-	 * @Param lastAirportInDestCtry
-	 * 				The last airport in destination country
-	 * @return int
-	 * 				The number of flight legs		
+	 *            The next airport in destination country
+	 * @Param lastAirportInDestCtry The last airport in destination country
+	 * @return int The number of flight legs
 	 *
 	 */
-	
-	public static Long getNumberOfFlightLegs(String firstAirportInDestCtry,String nextAirportInDestCtry,String lastAirportInDestCtry)
-	{
+
+	public static Long getNumberOfFlightLegs(String firstAirportInDestCtry, String nextAirportInDestCtry,
+			String lastAirportInDestCtry) {
 		Long numberOfFlightLeg = Long.valueOf(0L);
-		
-		if( firstAirportInDestCtry!=null && !firstAirportInDestCtry.isEmpty() )
+
+		if (firstAirportInDestCtry != null && !firstAirportInDestCtry.isEmpty())
 			numberOfFlightLeg = numberOfFlightLeg + 1;
-		if( nextAirportInDestCtry!=null && !nextAirportInDestCtry.isEmpty() )
+		if (nextAirportInDestCtry != null && !nextAirportInDestCtry.isEmpty())
 			numberOfFlightLeg = numberOfFlightLeg + 1;
-		if( lastAirportInDestCtry!=null && !lastAirportInDestCtry.isEmpty() )
+		if (lastAirportInDestCtry != null && !lastAirportInDestCtry.isEmpty())
 			numberOfFlightLeg = numberOfFlightLeg + 1;
-		
+
 		return numberOfFlightLeg;
 	}
-	
-    
-    /**
+
+	/**
 	 * @param flightDesignator
 	 *            Flight designator a.k.a carrier code + flight number
-	 *            
-	 * @return java.lang.String
-	 *			The character portion of the flightDesignator input, which is the carrier code.
+	 * 
+	 * @return java.lang.String The character portion of the flightDesignator input,
+	 *         which is the carrier code.
 	 */
-    public static String getFlightNumber(String flightDesignator) {       
-        
-    	String flightNumber = "";
-    	char c ;
-    	boolean isDigit = false;
-    	
-    	if(flightDesignator!=null && !flightDesignator.isEmpty())
-    	{
-    		for(int i=0;i<flightDesignator.length();i++)
-    		{
-    			
-    			c = flightDesignator.charAt(i);
-    			isDigit = Character.isDigit(c);
-    			if(isDigit)
-    			{
-    				flightNumber = flightNumber + c;
-    			}
-    			
-    		}
-    		
-    	}
-    	
-    	if(flightNumber.isEmpty())
-    	{
-    		flightNumber = null;
-    	}
-    	
-    	return flightNumber;
-    }
-    
-    /**
+	public static String getFlightNumber(String flightDesignator) {
+
+		String flightNumber = "";
+		char c;
+		boolean isDigit = false;
+
+		if (flightDesignator != null && !flightDesignator.isEmpty()) {
+			for (int i = 0; i < flightDesignator.length(); i++) {
+
+				c = flightDesignator.charAt(i);
+				isDigit = Character.isDigit(c);
+				if (isDigit) {
+					flightNumber = flightNumber + c;
+				}
+
+			}
+
+		}
+
+		if (flightNumber.isEmpty()) {
+			flightNumber = null;
+		}
+
+		return flightNumber;
+	}
+
+	/**
 	 * @param flightDesignator
 	 *            Flight designator a.k.a carrier code + flight number
-	 *            
-	 * @return ava.lang.String
-	 *			The character portion of the flightDesignator input, which is the carrier code.
+	 * 
+	 * @return ava.lang.String The character portion of the flightDesignator input,
+	 *         which is the carrier code.
 	 */
-    public static String getCarrierCode(String flightDesignator) {       
-        
-    	String carrierCode = "";
-    	char c ;
-    	boolean isDigit = false;
-    	
-    	if(flightDesignator!=null && !flightDesignator.isEmpty())
-    	{
-    		for(int i=0;i<flightDesignator.length();i++)
-    		{
-    			
-    			c = flightDesignator.charAt(i);
-    			isDigit = Character.isDigit(c);
-    			if(!isDigit)
-    			{
-    				carrierCode = carrierCode + c;
-    			}
-    			
-    		}
-    		
-    	}
-    	
-    	if(carrierCode.isEmpty())
-    	{
-    		carrierCode = null;
-    	}
-    	
-    	return carrierCode;
-    }
+	public static String getCarrierCode(String flightDesignator) {
+
+		String carrierCode = "";
+		char c;
+		boolean isDigit = false;
+
+		if (flightDesignator != null && !flightDesignator.isEmpty()) {
+			for (int i = 0; i < flightDesignator.length(); i++) {
+
+				c = flightDesignator.charAt(i);
+				isDigit = Character.isDigit(c);
+				if (!isDigit) {
+					carrierCode = carrierCode + c;
+				}
+
+			}
+
+		}
+
+		if (carrierCode.isEmpty()) {
+			carrierCode = null;
+		}
+
+		return carrierCode;
+	}
 }
--- gtas-parent/gtas-pdi-parser/src/main/java/gov/gtas/parser/pentaho/util/Constants.java
+++ gtas-parent/gtas-pdi-parser/src/main/java/gov/gtas/parser/pentaho/util/Constants.java
@@ -5,12 +5,11 @@ package gov.gtas.parser.pentaho.util;
  *
  */
 public class Constants {
-	
+
 	public static final String DATE_FORMAT_YYMMDD = "yyMMdd";
 	public static final String DATE_FORMAT_YYMMDDHHMM = "yyMMddHHmm";
 	public static final String DATE_FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
 	public static final String DATE_FORMAT_YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm:SS";
 	public static final String YYMMDDHHMM_CODE = "201";
-	 
-	
+
 }
