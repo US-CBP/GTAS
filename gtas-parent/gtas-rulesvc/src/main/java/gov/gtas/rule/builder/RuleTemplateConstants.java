47286178462dbca6820358373f6caf04aaff6fc6
Shawn Sheldon <michael.sheldon@unisys.com>
2019-10-10 20:26:18 -0500

    make members protected and remove unused set

--- gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/builder/RuleTemplateConstants.java
+++ gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/builder/RuleTemplateConstants.java
@@ -10,7 +10,7 @@ import java.util.*;
 import gov.gtas.constant.RuleServiceConstants;
 
 public class RuleTemplateConstants {
-    public static final String SEAT_ENTITY_NAME = "Seat";
+    static final String SEAT_ENTITY_NAME = "Seat";
     public static final String SEAT_ENTITY_NAME_LOWERCASE = "seat";
     public static final String SEAT_ATTRIBUTE_NAME = "number";
     public static final String DOLLAR_SIGN = "$";
@@ -76,32 +76,32 @@ public class RuleTemplateConstants {
         flightMutableDetailsMap = Collections.unmodifiableMap(flightDetailsMap);
     }
 
-    public static final String PASSENGER_TRIP_DETAILS_NAME = "PassengerTripDetails";
+    static final String PASSENGER_TRIP_DETAILS_NAME = "PassengerTripDetails";
 
 
-    public static final String PASSENGER_VARIABLE_NAME = "$p";
-    public static final String PASSENGER_DETAILS_VARIABLE_NAME = "$pcb";
-    public static final String PASSENGER_TRIP_VARIABLE_NAME = "$ptcb";
+    static final String PASSENGER_VARIABLE_NAME = "$p";
+    static final String PASSENGER_DETAILS_VARIABLE_NAME = "$pcb";
+    static final String PASSENGER_TRIP_VARIABLE_NAME = "$ptcb";
     public static final String DOCUMENT_VARIABLE_NAME = "$d";
-    public static final String FLIGHT_VARIABLE_NAME = "$f";
+    static final String FLIGHT_VARIABLE_NAME = "$f";
     public static final String ADDRESS_VARIABLE_NAME = "$addr";
     public static final String PHONE_VARIABLE_NAME = "$ph";
     public static final String EMAIL_VARIABLE_NAME = "$e";
     public static final String CREDIT_CARD_VARIABLE_NAME = "$cc";
     public static final String TRAVEL_AGENCY_VARIABLE_NAME = "$ta";
     public static final String FREQUENT_FLYER_VARIABLE_NAME = "$ff";
-    public static final String PNR_VARIABLE_NAME = "$pnr";
-    public static final String SEAT_VARIABLE_NAME = "$seat";
+    static final String PNR_VARIABLE_NAME = "$pnr";
+    static final String SEAT_VARIABLE_NAME = "$seat";
     public static final String PNR_SEAT = "$pnr_seat";
     public static final String DWELL_TIME_VARIABLE_NAME = "$dwell";
-    public static final String FLIGHT_PAX_VARIABLE_NAME = "$fp";
+    static final String FLIGHT_PAX_VARIABLE_NAME = "$fp";
     public static final String BAG_VARIABLE_NAME = "$b";
     public static final String LINK_VARIABLE_SUFFIX = "link";
     public static final String LINK_PNR_ID = "pnrId";
     public static final String LINK_ATTRIBUTE_ID = "linkAttributeId";
     public static final String BOOKING_DETAIL_VARIABLE_NAME = "$bl";
     public static final String PAYMENT_FORM_VARIABLE_NAME = "$pf";
-    public static final String PAYMENT_FORM_ENTITY_NAME = "PaymentForm";
+    static final String PAYMENT_FORM_ENTITY_NAME = "PaymentForm";
     public static final String PAYMENT_TYPE_ATTRIBUTE_NAME = "paymentType";
     public static final String PAYMENT_FORM_FIELD_ALIAS = "paymentForms";
     public static final String PAYMENT_FORM_FIELD_ALIAS_LOWERCASE = "paymentforms";
@@ -109,30 +109,23 @@ public class RuleTemplateConstants {
     private RuleTemplateConstants() {
         // to prevent instantiation.
     }
-    public static final Set<String> YES_SET;
-    static{
-        YES_SET = new HashSet<String>();
-        for(String member: new String[]{"Y", "y", "Yes", "YES"}){
-            YES_SET.add(member);
-        }
-        
-    }
+
     public static final char COLON_CHAR = ':';
-    public static final char DOUBLE_QUOTE_CHAR = '"';
+    static final char DOUBLE_QUOTE_CHAR = '"';
     public static final char SINGLE_QUOTE_CHAR = '\'';
     public static final char LEFT_PAREN_CHAR = '(';
-    public static final char RIGHT_PAREN_CHAR = ')';
-    public static final char SPACE_CHAR = ' ';
-    public static final char COMMA_CHAR = ',';
+    static final char RIGHT_PAREN_CHAR = ')';
+    static final char SPACE_CHAR = ' ';
+    static final char COMMA_CHAR = ',';
 
-    public static final String REGEX_WILDCARD = ".*";
+    static final String REGEX_WILDCARD = ".*";
 
     public static final String NEW_LINE = "\n";
-    public static final String TRUE_STRING = "true";
-    public static final String FALSE_STRING = "false";
+    static final String TRUE_STRING = "true";
+    static final String FALSE_STRING = "false";
         
-    public static final String RULE_PACKAGE_NAME = "package gov.gtas.rule;\n";
-    public static final String IMPORT_PREFIX = "import ";
-    public static final String GLOBAL_RESULT_DECLARATION = "global java.util.List "+RuleServiceConstants.RULE_RESULT_LIST_NAME+";\n\n";
+    static final String RULE_PACKAGE_NAME = "package gov.gtas.rule;\n";
+    static final String IMPORT_PREFIX = "import ";
+    static final String GLOBAL_RESULT_DECLARATION = "global java.util.List "+RuleServiceConstants.RULE_RESULT_LIST_NAME+";\n\n";
     
 }
