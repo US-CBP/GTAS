7fce3b7671e4f32acb17e815c11b965ada4c65a2
simbam1 <simbam1@umbc.edu>
2019-11-01 15:28:31 -0400

    email refactor

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/GtasEmailServiceController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/GtasEmailServiceController.java
@@ -1,31 +1,31 @@
 package gov.gtas.controller;
 
+import freemarker.template.TemplateException;
+import gov.gtas.services.NotificatonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.gtas.model.User;
-import gov.gtas.repository.UserRepository;
 import gov.gtas.security.service.GtasSecurityUtils;
-import gov.gtas.services.GtasEmailService;
+
+import javax.mail.MessagingException;
+import javax.transaction.Transactional;
+
+import java.io.IOException;
 
 @RestController
 public class GtasEmailServiceController {
 
 	@Autowired
-	private GtasEmailService emailService;
-	
-	@Autowired
-	UserRepository userRepository;
+	private NotificatonService notificationService;
 
+	@Transactional
 	@RequestMapping(value = "/notify", method = RequestMethod.POST)
-	public void sendEmail(@RequestParam String [] to, @RequestParam String paxId, @RequestParam String note) {
+	public void sendEmail(@RequestParam String [] to, @RequestParam Long paxId, @RequestParam String note) throws IOException, TemplateException, MessagingException {
 		String userId = GtasSecurityUtils.fetchLoggedInUserId();
-		User loggedInUser = userRepository.userAndGroups(userId).orElseThrow(RuntimeException::new);
-		emailService.send(to, Long.parseLong(paxId), note, loggedInUser);
-
+		notificationService.sendManualNotificationEmail(to, note, paxId, userId);
 	}
 
 }
