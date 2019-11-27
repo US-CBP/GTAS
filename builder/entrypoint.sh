c516b48f4b7170fbb7f717df992b3fcdad8055c8
simbam1 <simbam1@umbc.edu>
2019-11-27 12:09:44 -0500

    removing print statement

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -5,7 +5,6 @@ BUILD_NUMBER=SNAPSHOT
 WEBAPP_WAR="//root/.m2/repository/gov/gtas/gtas-webapp/$VERSION_NUMBER-BUILD-$BUILD_NUMBER/gtas-webapp-$VERSION_NUMBER-BUILD-$BUILD_NUMBER.war"
 
 if test -f "$WEBAPP_WAR"; then
-	cat /gtas-parent/gtas-commons/src/main/resources/default.application.properties
 	echo "gtas-webapp-$VERSION_NUMBER-BUILD-$BUILD_NUMBER.war already exists"
 else
 	cp default.application.properties /gtas-parent/gtas-commons/src/main/resources/default.application.properties
