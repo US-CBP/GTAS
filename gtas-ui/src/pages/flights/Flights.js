c84cc98bf364dcb155938b500b45f176bdf7beb9
Kasim Helil <kasimhelil@gmail.com>
2019-12-26 11:16:55 -0500

    Updated flight filter form

--- gtas-ui/src/pages/flights/Flights.js
+++ gtas-ui/src/pages/flights/Flights.js
@@ -6,13 +6,15 @@ import { Link } from "@reach/router";
 import LabelledInput from "../../components/labelledInput/LabelledInput";
 import FilterForm from "../../components/filterForm/FilterForm";
 import { store } from '../../appContext';
-import {hasData} from "../../utils/text";
+import { hasData } from "../../utils/text";
+import "./Flights.css";
+import { Nav, Container, Row, Col } from "react-bootstrap"
 
 const Flights = props => {
-  const cb = () => {};
+  const cb = () => { };
 
   const parameterAdapter = (fields) => {
-    let paramObject = {pageSize: 10, pageNumber:1};
+    let paramObject = { pageSize: 10, pageNumber: 1 };
     const fieldNames = Object.keys(fields);
     fieldNames.forEach(name => {
       if (hasData(fields[name])) {
@@ -23,7 +25,7 @@ const Flights = props => {
   };
 
 
-  const [data, setData] = useState({flights: []});
+  const [data, setData] = useState({ flights: [] });
 
   const globalState = useContext(store);
 
@@ -34,88 +36,86 @@ const Flights = props => {
   }, [globalState]);
 
   return (
-    <div className="container">
-      <Title title="Flights" uri={props.uri}/>
-      <div className="columns">
-        <div className="column is-3">
-          <div className="box2">
-            <aside className="menu">
-              <FilterForm
-                  service={flights}
-                  title="Filter"
-                  callback={setData}
-                  paramAdapter={parameterAdapter}
-              >
-                <hr/>
-                <LabelledInput
-                  datafield
-                  labelText="Origin Airport"
-                  inputType="text"
-                  name="origin"
-                  callback={cb}
-                  alt="nothing"
-                />
-                <LabelledInput
-                  datafield
-                  labelText="Destination Airport"
-                  inputType="text"
-                  name="destination"
-                  callback={cb}
-                  alt="nothing"
-                />
-                <LabelledInput
-                  datafield="flightNumber"
-                  labelText="Flight ID"
-                  inputType="number"
-                  name="flightId"
-                  callback={cb}
-                  alt="nothing"
-                />
-                <LabelledInput
-                  datafield="direction"
-                  labelText="Direction"
-                  inputType="text"
-                  name="direction"
-                  callback={cb}
-                  alt="nothing"
-                />
-                <LabelledInput
-                  datafield="etaStart"
-                  labelText="Start Date"
-                  inputType="text"
-                  name="departure"
-                  callback={cb}
-                  alt="nothing"
-                />
-                <LabelledInput
-                  datafield="etaEnd"
-                  labelText="End Date"
-                  inputType="text"
-                  name="arrival"
-                  callback={cb}
-                  alt="nothing"
-                />
-              </FilterForm>
-            </aside>
+    <Container fluid>
+      <Row>
+        <Col lg="2" md="3" sm="3">
+          <div className="flight-filter-nav">
+            <FilterForm
+              service={flights}
+              title="Filter"
+              callback={setData}
+              paramAdapter={parameterAdapter}
+            >
+              <hr />
+              <LabelledInput
+                datafield
+                labelText="Origin Airport"
+                inputType="text"
+                name="origin"
+                callback={cb}
+                alt="nothing"
+              />
+              <LabelledInput
+                datafield
+                labelText="Destination Airport"
+                inputType="text"
+                name="destination"
+                callback={cb}
+                alt="nothing"
+              />
+              <LabelledInput
+                datafield="flightNumber"
+                labelText="Flight ID"
+                inputType="number"
+                name="flightId"
+                callback={cb}
+                alt="nothing"
+              />
+              <LabelledInput
+                datafield="direction"
+                labelText="Direction"
+                inputType="text"
+                name="direction"
+                callback={cb}
+                alt="nothing"
+              />
+              <LabelledInput
+                datafield="etaStart"
+                labelText="Start Date"
+                inputType="text"
+                name="departure"
+                callback={cb}
+                alt="nothing"
+              />
+              <LabelledInput
+                datafield="etaEnd"
+                labelText="End Date"
+                inputType="text"
+                name="arrival"
+                callback={cb}
+                alt="nothing"
+              />
+            </FilterForm>
           </div>
-        </div>
+        </Col>
 
-        <div className="column">
-          <div className="box2">
+        <Col lg="10" md="9" sm="9" className="flight-body">
+          <Title title="Flights" uri={props.uri} />
+          <div className="flight-body-box">
             <Link to="../flightpax">Flight Passengers</Link>
             <div className="card events-card">
               <Table
-                  data={data.flights}
-                  id="Flights"
-                  callback={cb}
-                  key={data}
-                  ignoredFields = {["countDown"]}
+                data={data.flights}
+                id="Flights"
+                callback={cb}
+                key={data}
+                ignoredFields={["countDown"]}
               />
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </Col >
+      </Row>
+    </Container>
   );
 };
 
