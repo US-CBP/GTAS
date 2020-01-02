6fdd91178fa69ee5d17e32ae2b8d4686da3458cc
Kasim Helil <kasimhelil@gmail.com>
2020-01-02 11:28:50 -0500

    Removed bulma dependencies from dashboard page

--- gtas-ui/src/pages/dashboard/Dashboard.js
+++ gtas-ui/src/pages/dashboard/Dashboard.js
@@ -2,21 +2,15 @@ import React from "react";
 import Table from "../../components/table/Table";
 import { employees } from "../../services/serviceWrapper";
 import Title from "../../components/title/Title";
+import { Container } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
-    <div className="container">
-      <Title title="Dashboard"></Title>
-
-      <div className="columns">
-        <div className="column">
-          <div className="box2">
-            <div className="top">
-              <Table service={employees.get} id="foo"></Table>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Container><Title title="Dashboard"></Title></Container>
+      <Container fluid className="box2">
+        <Table service={employees.get} id="foo"></Table>
+      </Container>
     </div>
   );
 };
