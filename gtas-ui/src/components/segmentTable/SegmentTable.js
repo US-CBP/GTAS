0a343b3d7f4e2029a780da13f32441957c933417
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2020-01-03 14:56:30 -0500

    SegmentTable component
    Highlights and scrolls to records.

--- gtas-ui/src/components/segmentTable/SegmentTable.js
+++ gtas-ui/src/components/segmentTable/SegmentTable.js
@@ -1,97 +1,80 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { hasData, titleCase } from "../../utils/text";
+import { hasData } from "../../utils/text";
+import RBTable from "react-bootstrap/Table";
+import "./SegmentTable.css";
+// import SegmentRow from "./SegmentRow";
 
-//Will auto-populate with data retrieved from the given uri
-//Attempts to format the header from the column names, but can be passed a header array instead.
-class SegmentTable extends Component {
+/**
+ * Table for PNR data segments. Highlights and scrolls to rows with a class matching
+ * the string passed into setActiveKey by the parent container.
+ */
+
+// Pass in the color as a prop?
+class SegmentTable extends React.Component {
   constructor(props) {
     super(props);
-    this.onSort = this.onSort.bind(this);
 
-    this.validateProps();
+    let nodata = {};
+    nodata[props.id] = "No Data Found";
 
+    let dataArray = Array.isArray(props.data) ? props.data : [props.data];
+    const sdata = hasData(dataArray) ? dataArray : [nodata];
+    const sheader = hasData(dataArray) ? Object.keys(dataArray[0]) : [props.id];
+
+    const refs = sdata.reduce((acc, rec) => {
+      acc[rec.key] = React.createRef();
+      return acc;
+    }, {});
+
+    const color = `${props.color || "yellow"}`;
     this.state = {
-      data: [],
-      header: []
+      data: sdata,
+      header: sheader,
+      refs: refs,
+      color: color
     };
   }
 
-  validateProps() {
-    if (!Array.isArray(this.props.data)) {
-      const err = new Error("Table requires a data prop");
-      throw err;
+  setActiveKey = key => {
+    this.setState({ activeKey: key });
+    const row = this.state.refs[key];
+
+    if (row) {
+      row.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest"
+      });
     }
-  }
-
-  componentDidMount() {
-    if (!Array.isArray(this.props.data)) this.getData();
-    else this.parseData(this.props.data);
-  }
-
-  parseData(data) {
-    let noDataObj = {};
-    noDataObj[this.props.id] = "No Data Found";
-
-    let dataArray = Array.isArray(data) ? data : [data];
-    const sdata = hasData(dataArray) ? dataArray : [noDataObj];
-    const sheader = hasData(dataArray)
-      ? this.props.header || Object.keys(dataArray[0])
-      : [this.props.id];
-
-    this.setState({
-      data: sdata,
-      header: sheader
-    });
-  }
-
-  createTable = () => {
-    let table = [];
-    let header = [];
-
-    // header
-    for (let field of this.state.header) {
-      header.push(
-        <th scope="col" key={field} onClick={this.onSort}>
-          {titleCase(field)}
-        </th>
-      );
-    }
-    table.push(
-      <React.Fragment>
-        <thead key={"header"}>
-          <tr>{header}</tr>
-        </thead>
-        <tbody>{this.createRows()}</tbody>
-      </React.Fragment>
-    );
-
-    return table;
-  };
-
-  createRows = () => {
-    const stateData = this.state.data;
-    let rows = [];
-    for (let raw of stateData) {
-      let rec = JSON.parse(JSON.stringify(raw, this.state.header, 4));
-
-      let children = [];
-      rows.push(<tr key={rec[this.props.id]}>{children}</tr>);
-    }
-    return rows;
   };
 
   render() {
+    let idx = 0;
     return (
-      <div className="card-table">
-        <div className="content">
-          {this.props.title !== undefined && (
-            <h4 className={`title ${this.props.style}`}>{this.props.title}</h4>
-          )}
-          {this.props.smalltext !== undefined && <small>{this.props.smalltext}</small>}
+      <div className="tableHeight">
+        {this.props.title && (
+          <h4 className={`title ${this.props.style}`}>{this.props.title}</h4>
+        )}
 
-          <table className="table is-fullwidth is-striped">{this.createTable()}</table>
-        </div>
+        <RBTable striped bordered hover size="sm" responsive>
+          <tbody>
+            {this.state.data.map(raw => {
+              const rec = JSON.parse(JSON.stringify(raw, this.state.header, 4));
+              const active = this.state.activeKey === +rec.key ? " highlight" : "";
+
+              return (
+                <tr
+                  key={idx}
+                  className={`${rec.key}${active}`}
+                  ref={this.state.refs[rec.key]}
+                >
+                  <td className={rec.key}>{idx++}</td>
+                  <td className={rec.key}>{rec.value}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </RBTable>
       </div>
     );
   }
@@ -99,12 +82,8 @@ class SegmentTable extends Component {
 
 SegmentTable.propTypes = {
   id: PropTypes.string.isRequired,
-  callback: PropTypes.func.isRequired,
-  service: PropTypes.func,
-  uri: PropTypes.string,
-  data: PropTypes.array,
-  header: PropTypes.array,
-  title: PropTypes.string,
-  smalltext: PropTypes.string,
-  style: PropTypes.string
+  data: PropTypes.array.isRequired,
+  title: PropTypes.string
 };
+
+export default SegmentTable;
