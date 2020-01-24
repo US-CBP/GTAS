4154f2e2c24da5cf4d2eef768d33be1b3f9b87c7
Cassidy Laidlaw <cassidy256@gmail.com>
2020-01-24 16:43:00 -0500

    Remove unused code and imports

--- gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/tamr/jms/TamrQueueConfig.java
+++ gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/tamr/jms/TamrQueueConfig.java
@@ -5,25 +5,17 @@
  */
 package gov.gtas.parsers.tamr.jms;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Session;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.destination.DestinationResolver;
 
-//Uncomment in order in part to re-enable queues
 @Configuration
 @ConditionalOnProperty(prefix = "tamr", name = "enabled")
 @EnableJms
@@ -38,16 +30,6 @@ public class TamrQueueConfig {
     public JmsListenerContainerFactory<?> tamrJmsListenerContainerFactory() {        
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         
-//        factory.setDestinationResolver(new DestinationResolver() {
-//            @Override
-//            public Destination resolveDestinationName(Session session, String destinationName, boolean pubSubDomain)
-//                    throws JMSException {
-//                logger.info("session = {}, destinationName = {}, pubSubDomain = {}",
-//                        session, destinationName, pubSubDomain);
-//                // Create a destination
-//                return session.createQueue("OutboundQueue");
-//            }
-//        });
         factory.setConnectionFactory(
                 new ActiveMQConnectionFactory(activeMQBrokerUrl));
         factory.setSessionTransacted(true);
