b38e2728f371e5a2ff0ec99cd3c9a61196a1e0de
Robert Stine <RobWStine@outlook.com>
2020-03-24 13:54:51 -0400

    Add sql to remove doc id hash, add tamr id hash, add pending hit detail table.

---
+++ gtas-parent/scripts/db/1.10.0_to_1.11.0/tamr_id_index_addition.sql
@@ -0,0 +1,10 @@
+/*
+ *
+ *  * All Application code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+ *  *
+ *  * Please see LICENSE.txt for details.
+ *
+ */
+
+create or replace index tamr_id_index
+    on passenger_id_tag (tamr_id);
