35f9a0a51bee272118b12184f2e519b8c89f3f05
Teddy <Teddy@192.168.86.122>
2020-04-10 10:06:01 -0400

    Fixed code to show the user session expired popup message

--- gtas-parent/gtas-webapp/src/main/webapp/login/LoginController.js
+++ gtas-parent/gtas-webapp/src/main/webapp/login/LoginController.js
@@ -18,9 +18,9 @@
                 $mdDialog.show(
                         $mdDialog.alert()
                             .clickOutsideToClose(false)
-                            .textContent("{{'msg.sessiontimeout' | translate}}")
-                            .ariaLabel("{{'msg.usertimeout' | translate}}")
-                            .ok("{{'msg.ok' | translate}}")
+                            .textContent("Your session has timed out. For security reasons you have been logged out automatically.")
+                            .ariaLabel("User Timeout")
+                            .ok("OK")
                             .openFrom({
                                 left: 1500
                             })
