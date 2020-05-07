d636fc33ad3bf22d8139015ab29901c34e30a725
Kasim Helil <kasimhelil@gmail.com>
2020-05-07 16:21:31 -0400

    Cleaning up redundant variable references

--- gtas-ui/src/components/accordion/Accordion.js
+++ gtas-ui/src/components/accordion/Accordion.js
@@ -4,13 +4,13 @@ import { Accordion as RBAccordion } from "react-bootstrap";
 import "./Accordion.scss";
 
 const Accordion = props => {
-  const getCard = cardData => {
+  const getContent = cardData => {
     return cardData.map(data => {
       return <Content header={data.header} body={data.body} key={data.header} />;
     });
   };
 
-  return <RBAccordion>{getCard(props.data)}</RBAccordion>;
+  return <RBAccordion>{getContent(props.data)}</RBAccordion>;
 };
 
 export default Accordion;
--- gtas-ui/src/components/accordion/content/Content.js
+++ gtas-ui/src/components/accordion/content/Content.js
@@ -4,14 +4,16 @@ import Toggle from "../toggle/Toggle";
 
 const AccordionContent = props => {
   const cb = () => {};
+  const header = props.header;
+  const body = props.body;
 
   return (
-    <Card className="accordion-card" key={props.header}>
-      <Toggle eventKey={props.header} callback={cb}>
-        {props.header}
+    <Card className="accordion-card" key={header}>
+      <Toggle eventKey={header} callback={cb}>
+        {header}
       </Toggle>
-      <RBAccordion.Collapse eventKey={props.header}>
-        <Card.Body>{props.body}</Card.Body>
+      <RBAccordion.Collapse eventKey={header}>
+        <Card.Body>{body}</Card.Body>
       </RBAccordion.Collapse>
     </Card>
   );
