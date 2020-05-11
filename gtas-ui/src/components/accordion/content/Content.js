e5d1784ff1f999c9e21813849e153dc8a2e927c0
Kasim Helil <kasimhelil@gmail.com>
2020-05-11 11:29:37 -0400

    Updates on accordion including header color

--- gtas-ui/src/components/accordion/content/Content.js
+++ gtas-ui/src/components/accordion/content/Content.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Accordion as RBAccordion } from "react-bootstrap";
+import { Card, Accordion as RBAccordion, Col } from "react-bootstrap";
 import Toggle from "../toggle/Toggle";
 
 const AccordionContent = props => {
@@ -8,14 +8,16 @@ const AccordionContent = props => {
   const body = props.body;
 
   return (
-    <Card className="accordion-card" key={header}>
-      <Toggle eventKey={header} callback={cb}>
-        {header}
-      </Toggle>
-      <RBAccordion.Collapse eventKey={header}>
-        <Card.Body>{body}</Card.Body>
-      </RBAccordion.Collapse>
-    </Card>
+    <Col xs={12} sm={6}>
+      <Card className="accordion-card" key={header}>
+        <Toggle eventKey={header} callback={cb}>
+          {header}
+        </Toggle>
+        <RBAccordion.Collapse eventKey={header}>
+          <Card.Body>{body}</Card.Body>
+        </RBAccordion.Collapse>
+      </Card>
+    </Col>
   );
 };
 
