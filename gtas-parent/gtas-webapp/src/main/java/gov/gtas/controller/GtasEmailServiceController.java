1f6e9447e451bf3eaf69e6180c2a233f39c797e2
Kasim Helil <kasimhelil@gmail.com>
2020-06-25 12:59:49 -0400

    Added a new end point for user notification
    - Added a DTO for notification request body
    - the old end point takes RequestParams, where as the current one takes
    RequestBody as a parameter for post request

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/GtasEmailServiceController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/GtasEmailServiceController.java
@@ -4,12 +4,16 @@ import gov.gtas.services.NotificatonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import gov.gtas.email.dto.EmailNotificationDTO;
 import gov.gtas.security.service.GtasSecurityUtils;
 
 import javax.transaction.Transactional;
@@ -32,6 +36,20 @@ public class GtasEmailServiceController {
 		String userId = GtasSecurityUtils.fetchLoggedInUserId();
 		try {
 			notificationService.sendManualNotificationEmail(to, note, paxId, userId);
+			return new ResponseEntity<>(OK);
+		} catch(Exception ex) {
+			LOGGER.error(format("Manual email notification from UserId (%s) to failed with the exception: %s", userId, ex.toString()));
+			return new ResponseEntity<>(BAD_REQUEST);
+		}
+	}
+	
+	@Transactional
+	@PostMapping(value = "users/notify", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> sendEmail(@RequestBody EmailNotificationDTO emialInfo) {
+		
+		String userId = GtasSecurityUtils.fetchLoggedInUserId();
+		try {
+			notificationService.sendManualNotificationEmail(emialInfo.getTo(), emialInfo.getNote(), emialInfo.getPaxId(), userId);
 			return new ResponseEntity<>(OK);
 		} catch(Exception ex) {
 			LOGGER.error(format("Manual email notification from UserId (%s) to failed with the exception: %s", userId, ex.toString()));
