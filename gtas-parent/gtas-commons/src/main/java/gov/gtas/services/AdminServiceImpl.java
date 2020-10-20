f1ad894ff410876e853769a87065fcbd35b134a5
Kasim Helil <kasimhelil@gmail.com>
2020-10-20 16:05:03 -0400

    Clean up application statistics data
    - created a new dto that consists only fields required by the front end
    
    - Updated all returned date types to Long so that the front end can
    parse them correctly
    
    - Updated total loading error to include DUPLICATE_MESSAGE (this was
    throwing error on the prev code and the fetch was failing)

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/services/AdminServiceImpl.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/services/AdminServiceImpl.java
@@ -12,6 +12,7 @@ import gov.gtas.model.HitDetail;
 import gov.gtas.model.Message;
 import gov.gtas.model.MessageStatus;
 import gov.gtas.repository.HitDetailRepository;
+import gov.gtas.services.dto.AppStatisticsDTO;
 import gov.gtas.services.dto.ApplicationStatisticsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -34,6 +35,7 @@ public class AdminServiceImpl implements AdminService {
 		this.hitDetailRepository = hitDetailRepository;
 	}
 
+	@Deprecated
 	@Override
 	public ApplicationStatisticsDTO createApplicationStatisticsDto() {
 		List<Message> messageList = messageService.getMostRecent500Messages();
@@ -54,6 +56,7 @@ public class AdminServiceImpl implements AdminService {
 		return applicationStatisticsDTO;
 	}
 
+	@Deprecated
 	@SuppressWarnings("WeakerAccess") // For test.
 	protected void populateApplicationStatistics(ApplicationStatisticsDTO applicationStatisticsDTO,
 			List<MessageStatus> msList) {
@@ -106,7 +109,7 @@ public class AdminServiceImpl implements AdminService {
 			}
 		}
 	}
-
+	
 	protected int getPassengerCount(List<Message> messageList) {
 		int count = 0;
 		for (Message m : messageList) {
@@ -115,5 +118,68 @@ public class AdminServiceImpl implements AdminService {
 			}
 		}
 		return count;
+	}
+	
+	@Override
+	public AppStatisticsDTO getAppStatistics() {
+
+		List<Message> messageList = messageService.getMostRecent500Messages();
+		if (messageList.isEmpty()) {
+			return new AppStatisticsDTO();
+		}
+		int passengerCount = getPassengerCount(messageList);
+		AppStatisticsDTO applicationStatisticsDTO = new AppStatisticsDTO();
+		applicationStatisticsDTO.setPassengerCount(passengerCount);
+		messageList.stream().findFirst()
+				.ifPresent(m -> applicationStatisticsDTO.setLastMessageInSystem(m.getCreateDate().getTime()));
+		
+		List<MessageStatus> msList = messageList.stream().map(Message::getStatus).collect(Collectors.toList());
+		
+		getApplicationStatistics(applicationStatisticsDTO, msList);
+		HitDetail mostRecentHitDetail = hitDetailRepository.findFirstByOrderByIdDesc();
+		
+		if (mostRecentHitDetail != null) {
+			applicationStatisticsDTO.setMostRecentRuleHit(mostRecentHitDetail.getCreatedDate().getTime());
+		}
+		return applicationStatisticsDTO;
+	}
+	
+	private void getApplicationStatistics(AppStatisticsDTO dto, List<MessageStatus> msList) {
+		dto.setLastMessageAnalyzedByDrools(Date.from(Instant.EPOCH).getTime());
+		
+		for (MessageStatus ms : msList) {
+			switch (ms.getMessageStatusEnum()) {
+			case ANALYZED:
+				if (ms.getAnalyzedTimestamp() != null
+				&& ms.getAnalyzedTimestamp().after(new Date(dto.getLastMessageAnalyzedByDrools()))) {
+					dto.setLastMessageAnalyzedByDrools(ms.getAnalyzedTimestamp().getTime());
+				}
+				break;
+			case FAILED_PARSING:
+				dto.setTotalLoadingParsingErrors(dto.getTotalLoadingParsingErrors() + 1);
+				break;
+			case FAILED_LOADING:
+				dto.setTotalLoadingParsingErrors(dto.getTotalLoadingParsingErrors() + 1);
+				break;
+			case DUPLICATE_MESSAGE:
+				dto.setTotalLoadingParsingErrors(dto.getTotalLoadingParsingErrors() + 1);
+				break;
+			case NEO_LOADED:
+				dto.setTotalRuleErros(dto.getTotalRuleErros() + 1);
+				break;
+			case FAILED_ANALYZING:
+				dto.setTotalRuleErros(dto.getTotalRuleErros() + 1);
+				break;
+			case PARTIAL_ANALYZE:
+				dto.setTotalRuleErros(dto.getTotalRuleErros() + 1);
+				break;
+			
+			default:
+				break;
+			
+			}
+		}
+		
+		
 	}
 }
