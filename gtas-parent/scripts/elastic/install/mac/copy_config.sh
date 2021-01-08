775c35e7c0101fc4d35aa8f2fc0185c301741c14
Tsegay <tsegay.tekle@gmail.com>
2021-01-08 15:56:17 -0500

    add missing environment variables to automate elk install

--- gtas-parent/scripts/elastic/install/mac/copy_config.sh
+++ gtas-parent/scripts/elastic/install/mac/copy_config.sh
@@ -13,4 +13,4 @@ echo $ES_INSTALL_LOCATION/logstash/config
 yes | cp -rf ../../config/logstash/* $ES_INSTALL_LOCATION/logstash/config
 yes | cp -rf ../../config/logstash/mac/* $ES_INSTALL_LOCATION/logstash/config
 
-yes | cp -f ../../../../../gtas-parent/gtas-webapp/target/gtas/WEB-INF/lib/mariadb-java-client-2.3.0.jar $ES_INSTALL_LOCATION/logstash/config
+wget https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar -P  $ES_INSTALL_LOCATION/logstash/logstash-core/lib/jars
--- gtas-parent/scripts/elastic/install/mac/logstash/start.sh
+++ gtas-parent/scripts/elastic/install/mac/logstash/start.sh
@@ -15,6 +15,11 @@ export SERVER_HOST='localhost'
 export DATABASE_HOST='localhost'
 export LOGSTASH_DIR=$ES_INSTALL_LOCATION/logstash
 export LOGSTASH_LIB=$LOGSTASH_DIR/
+export MARIADB_USER='root'
+export MARIADB_HOST='localhost'
+export MARIADB_PASSWORD='admin'
+
+export ES_PASSWORD=
 # Start Elstic Search
 
 ./bin/logstash & echo $! > ./pid
