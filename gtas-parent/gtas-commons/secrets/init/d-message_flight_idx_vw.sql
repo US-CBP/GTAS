3184ac275005fe21582122b99114fb0fef721246
simbam1 <simbam1@umbc.edu>
2021-03-08 16:58:36 -0500

    update report related view creation script

--- gtas-parent/gtas-commons/secrets/init/d-message_flight_idx_vw.sql
+++ gtas-parent/gtas-commons/secrets/init/d-message_flight_idx_vw.sql
@@ -11,7 +11,6 @@ f.flight_number as flight_number,
 f.full_flight_number as full_flight_number, 
 f.carrier as carrier, 
 f.direction as direction, 
-(select CONCAT(iata,'-',name) from airport where iata = f.destination) AS full_destination,
 (CASE
 	WHEN f.direction = 'I' THEN f.destination
 	WHEN f.direction = 'O' THEN f.origin
@@ -57,12 +56,15 @@ CAST(HOUR(CASE
 	WHEN f.direction = 'O' THEN f.etd_date
 END) AS UNSIGNED INTEGER)  AS flight_hour,
 
-f.origin as origin, 
-f.origin_country as origin_country, 
-f.destination as destination, 
+f.origin as origin,
+(select CONCAT(iata,'-',name) from airport where iata = f.origin) AS full_origin,
+f.origin_country as origin_country,
+f.destination as destination,
+(select CONCAT(iata,'-',name) from airport where iata = f.destination) AS full_destination,
 f.destination_country as destination_country,
+CONCAT(f.origin, ', ',  (select name from country where iso3 =f.origin_country),' - ',f.destination,', ',  (select name from country where iso3 = f.destination_country ) ) as route,
  mfd.full_utc_etd_timestamp as full_utc_etd_timestamp,
- mfd.full_utc_eta_timestamp as full_utc_eta_timestamp, 
+ mfd.full_utc_eta_timestamp as full_utc_eta_timestamp,
  CAST(CASE
 	WHEN f.direction = 'I' THEN fpc.fp_count
 	WHEN f.direction = 'O' THEN 0
@@ -73,7 +75,7 @@ CAST(CASE
 	WHEN f.direction = 'O' THEN fpc.fp_count
 END AS UNSIGNED  INTEGER) AS outbound_passenger_count,
 
- fpc.fp_count as passenger_count, 
+ fpc.fp_count as passenger_count,
 
  TIMESTAMPDIFF(HOUR, apm.transmission_date,
  (CASE
@@ -89,11 +91,11 @@ END)) as msg_trans_comp_mins
 
 FROM gtas.message msg
 INNER JOIN gtas.message_status mst ON msg.id = mst.ms_message_id
-INNER JOIN gtas.apis_message apm ON msg.id = apm.id 
-INNER JOIN gtas.apis_message_flight amf ON apm.id = amf.apis_message_id 
-INNER JOIN gtas.flight f ON amf.flight_id= f.id 
-INNER JOIN gtas.mutable_flight_details mfd ON f.id = mfd.flight_id 
-INNER JOIN  gtas.flight_passenger_count fpc ON fpc.fp_flight_id = f.id AND f.id_tag IS NOT NULL 
+INNER JOIN gtas.apis_message apm ON msg.id = apm.id
+INNER JOIN gtas.apis_message_flight amf ON apm.id = amf.apis_message_id
+INNER JOIN gtas.flight f ON amf.flight_id= f.id
+INNER JOIN gtas.mutable_flight_details mfd ON f.id = mfd.flight_id
+INNER JOIN  gtas.flight_passenger_count fpc ON fpc.fp_flight_id = f.id AND f.id_tag IS NOT NULL
 WHERE mst.ms_status NOT IN ('RECEIVED','PARSED','FAILED_PARSING','FAILED_LOADING','FAILED_PRE_PARSE')
 AND mst.message_flight_idx_flag IS NULL
 AND (f.direction = 'I' OR f.direction = 'O')
@@ -102,19 +104,18 @@ LIMIT 5000
 )
 
  UNION ALL
- 
-(SELECT 
+
+(SELECT
 msg.id,
 'PNR' as 'message_type',
-msg.create_date as message_create_dtm, 
-pnr.transmission_date as transmission_date, 
-f.id_tag as flight_id_tag, 
-f.id as flight_id, 
-f.flight_number as flight_number, 
-f.full_flight_number as full_flight_number, 
-f.carrier as carrier, 
+msg.create_date as message_create_dtm,
+pnr.transmission_date as transmission_date,
+f.id_tag as flight_id_tag,
+f.id as flight_id,
+f.flight_number as flight_number,
+f.full_flight_number as full_flight_number,
+f.carrier as carrier,
 f.direction as direction,
-(select CONCAT(iata,'-',name) from airport where iata = f.destination) AS full_destination,
 (CASE
 	WHEN f.direction = 'I' THEN f.destination
 	WHEN f.direction = 'O' THEN f.origin
@@ -159,11 +160,14 @@ CAST(HOUR(CASE
 	WHEN f.direction = 'O' THEN f.etd_date
 END) AS UNSIGNED  INTEGER)  AS flight_hour,
 
-f.origin as origin, 
-f.origin_country as origin_country, 
-f.destination as destination, 
-f.destination_country as destination_country, 
-mfd.full_utc_etd_timestamp as full_utc_etd_timestamp, 
+f.origin as origin,
+(select CONCAT(iata,'-',name) from airport where iata = f.origin) AS full_origin,
+f.origin_country as origin_country,
+f.destination as destination,
+(select CONCAT(iata,'-',name) from airport where iata = f.destination) AS full_destination,
+f.destination_country as destination_country,
+CONCAT(f.origin, ', ',  (select name from country where iso3 =f.origin_country),' - ',f.destination,', ',  (select name from country where iso3 = f.destination_country ) ) as route,
+mfd.full_utc_etd_timestamp as full_utc_etd_timestamp,
 mfd.full_utc_eta_timestamp as full_utc_eta_timestamp,
 CAST(CASE
 	WHEN f.direction = 'I' THEN fpc.fp_count
@@ -174,7 +178,7 @@ CAST(CASE
 	WHEN f.direction = 'I' THEN 0
 	WHEN f.direction = 'O' THEN fpc.fp_count
 END AS UNSIGNED  INTEGER) AS outbound_passenger_count,
- 
+
 fpc.fp_count as passenger_count,
 
 TIMESTAMPDIFF(HOUR, pnr.transmission_date,
@@ -189,18 +193,18 @@ TIMESTAMPDIFF(MINUTE, pnr.transmission_date,
 	WHEN f.direction = 'O' THEN mfd.full_utc_etd_timestamp
 END)) as msg_trans_comp_mins
 
-FROM gtas.message msg 
+FROM gtas.message msg
 INNER JOIN gtas.message_status mst ON msg.id = mst.ms_message_id
-INNER JOIN gtas.pnr pnr ON msg.id = pnr.id 
-INNER JOIN gtas.pnr_flight pfl ON pnr.id = pfl.pnr_id 
-INNER JOIN gtas.flight f ON pfl.flight_id = f.id 
-INNER JOIN gtas.mutable_flight_details mfd ON f.id = mfd.flight_id 
-INNER JOIN  gtas.flight_passenger_count fpc ON fpc.fp_flight_id = f.id AND f.id_tag IS NOT NULL 
+INNER JOIN gtas.pnr pnr ON msg.id = pnr.id
+INNER JOIN gtas.pnr_flight pfl ON pnr.id = pfl.pnr_id
+INNER JOIN gtas.flight f ON pfl.flight_id = f.id
+INNER JOIN gtas.mutable_flight_details mfd ON f.id = mfd.flight_id
+INNER JOIN  gtas.flight_passenger_count fpc ON fpc.fp_flight_id = f.id AND f.id_tag IS NOT NULL
 WHERE mst.ms_status NOT IN ('RECEIVED','PARSED','FAILED_PARSING','FAILED_LOADING','FAILED_PRE_PARSE')
 AND mst.message_flight_idx_flag IS NULL
 AND (f.direction = 'I' OR f.direction = 'O')
 ORDER BY msg.id, f.id
-LIMIT 25000
+LIMIT 10000
 )
 ORDER BY id ASC
-Limit 30000 
+Limit 15000
