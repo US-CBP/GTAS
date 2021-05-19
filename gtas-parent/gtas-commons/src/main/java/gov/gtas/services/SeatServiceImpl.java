6707dde47523f4a5a60d3646e64ba5073456aebc
Jon <taylorjwdev@gmail.com>
2021-05-19 10:11:31 -0400

    Adding APIS priority to Seat Service Call -This will prioritize APIS seats over PNR for the seat side bar return

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/services/SeatServiceImpl.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/services/SeatServiceImpl.java
@@ -1,6 +1,8 @@
 package gov.gtas.services;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -24,9 +26,25 @@ public class SeatServiceImpl implements SeatService {
 		List<Seat> seatList = seatRepository.findByFlightIdAndPassengerId(flightId, paxId);
 		List<String> seatNumber = null;
 		if (CollectionUtils.isNotEmpty(seatList)) {
-			List<String> seats = seatList.stream().map(seat -> seat.getNumber()).distinct()
+			//We check for APIS here as they are a priority return.
+			//If we have APIS seats, we assume these are the most up to date seats.
+			boolean hasApisSeat = false;
+			boolean hasPnrSeat = false;
+			Set<Seat> seatSet = new HashSet<>();
+			for(Seat seat: seatList){
+				if(seat.getApis()) { //If the seat is APIS add it to set
+					hasApisSeat = true;
+					if(hasPnrSeat){ //If the set has PNR seats in it, empty the set and add the APIS instead
+						seatSet.clear(); //This will only logically be done once, if at all.
+					}
+					seatSet.add(seat);
+				} else if(!hasApisSeat) { //Logically implies is not an Apis Seat.
+					hasPnrSeat = true;
+					seatSet.add(seat); //Only possible to add if not ApisSeat
+				}
+			}
+			seatNumber = seatSet.stream().map(Seat::getNumber).distinct()
 					.collect(Collectors.toList());
-			seatNumber = seats;
 		}
 
 		return seatNumber;
