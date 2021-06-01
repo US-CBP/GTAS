bb6c020c80c28bf09d92d9406c66c3ee824b91e3
Jon <taylorjwdev@gmail.com>
2021-06-01 16:15:23 -0400

    Fix POE date range, remove filter on poe fetch -Date range properly respects inbound/outbound flight times now. -POE will no longer filter "REVIEWED" status from the PVL side of the equation

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/HitViewStatusRepository.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/HitViewStatusRepository.java
@@ -42,11 +42,10 @@ public interface HitViewStatusRepository extends CrudRepository<HitViewStatus, L
             "left join fetch p.hitDetails hd " +
             "left join fetch hd.hitMaker hm "
     + " where hvs.userGroup in :userGroups "
-    + " AND NOT hvs.hitViewStatusEnum = 'REVIEWED' "
     + " AND NOT hvs.lookoutStatusEnum = 'NOTPROMOTED' "
     + " AND NOT hvs.lookoutStatusEnum = 'DEMOTED' "
-    + " AND (mf.etd BETWEEN :startDate AND :endDate "
-    + " OR mf.eta BETWEEN :startDate AND :endDate)")
+    + " AND ( (f.direction = 'O' AND mf.etd BETWEEN :startDate AND :endDate) "
+    + " OR (f.direction ='I' AND mf.eta BETWEEN :startDate AND :endDate ))")
     Set<HitViewStatus> findAllWithNotClosedAndWithinRange(@Param("userGroups") Set<UserGroup> userGroups,
                                                            @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
