045da45c88d0352756440ed58b0e384dfc77ee45
simbam1 <simbam1@umbc.edu>
2021-06-23 11:32:47 -0400

    Update flightpax_script.sql

--- gtas-parent/scripts/elastic/config/logstash/flightpax_script.sql
+++ gtas-parent/scripts/elastic/config/logstash/flightpax_script.sql
@@ -186,4 +186,4 @@
 		on (seat.flight_id=f.ID and seat.passenger_id=p.id)
  	where message.create_date >= (select IFNULL(min_id.val, :sql_last_value) from (select MIN(id) as val from message where create_date < (select create_date from message where id=:sql_last_value) and  create_date > (select create_date from message where id=:sql_last_value) - INTERVAL 3 MINUTE) min_id)
  	and message.id < :sql_last_value  + 200
- 	order by message.id DESC) apis_data order by message_id DESC
+ 	order by message.id DESC) apis_data order by message_id ASC
